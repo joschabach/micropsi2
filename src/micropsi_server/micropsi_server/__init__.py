5025415d53d6ce075ff2eca7d2e27ccb676f473f
Joscha Bach <joscha.bach@gmail.com>
2012-04-23 17:07:42 +0200

    Kotti user management visible on start page

--- src/micropsi_server/micropsi_server/__init__.py
+++ src/micropsi_server/micropsi_server/__init__.py
@@ -2,7 +2,6 @@ from pyramid.view import view_config
 from kotti import base_configure
 
 
-@view_config(route_name='start', renderer='micropsi_server:templates/start.pt')
 def start(request):
     return dict(title='Hallo, Sie sehen gut aus heute!')
 
@@ -11,6 +10,5 @@ def main(global_config, **settings):
     """ Configure and create the main application. """
     config = base_configure(global_config, **settings)
     config.add_static_view('static', 'micropsi_server:static')
-    config.add_route('start', '/')
     config.scan()
     return config.make_wsgi_app()

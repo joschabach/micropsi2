4bbf28c4013e32c1558a130bca4a1533ffaa26ac
Joscha Bach <joscha.bach@gmail.com>
2012-04-28 06:30:33 +0200

    Rendering of nodes using paper.js

--- src/micropsi_server/micropsi_server/__init__.py
+++ src/micropsi_server/micropsi_server/__init__.py
@@ -1,7 +1,7 @@
 from pyramid.view import view_config
 from kotti import base_configure
 
-
+@view_config(route_name="nodenet", renderer='micropsi_server:templates/nodespaceviewer.pt')
 def start(request):
     return dict(title='Hallo, Sie sehen gut aus heute!')
 
@@ -10,5 +10,6 @@ def main(global_config, **settings):
     """ Configure and create the main application. """
     config = base_configure(global_config, **settings)
     config.add_static_view('static', 'micropsi_server:static')
+    config.add_route('nodenet', '/nodenet')
     config.scan()
     return config.make_wsgi_app()

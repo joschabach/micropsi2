bdb3f8539ddfaa7e0c2b652db6276df9a06d57b3
Joscha Bach <joscha.bach@gmail.com>
2012-07-10 19:49:14 +0200

    Runtime will be monolithic now (instead of featuring two managers)

--- micropsi_core/runtime.py
+++ micropsi_core/runtime.py
@@ -9,41 +9,45 @@ maintains a set of users, worlds (up to one per user), and agents, and provides 
 __author__ = 'joscha'
 __date__ = '10.05.12'
 
-# import nodenet
-import world
+import environment
+import nodenet
 import os
 import tools
 
-RESOURCE_PATH = os.path.join(os.path.dirname(__file__),"..","resources")
+AGENT_DIRECTORY = "agents"
+WORLD_DIRECTORY = "worlds"
+
 
 class MicroPsiRuntime(object):
     """The central component of the MicroPsi installation.
 
-    The runtime instantiates a user manager, an agent manager and a world manager and coordinates the interaction
-    between them. It must be a singleton, otherwise competing instances might conflict over the resource files.
+    The runtime instantiates agents and worlds and coordinates the interaction
+    between them. It should be a singleton, otherwise competing instances might conflict over the resource files.
     """
+    def __init__(self, resource_path):
+        """Scan for existing node nets below the agent directory and set up a list of available agents.
 
-    def __init__(self):
-        pass
-
-
-AGENT_DIRECTORY = "agents"
-
-class AgentManager(object):
-    """Holds a number of agents (which are representated by node nets) and manages their persistency and execution"""
-
-    def __init__(self, resource_path = RESOURCE_PATH):
-        """Scans for existing node nets below the agent directory"""
+        Arguments:
+            resource_path: the path to the directory in which agent and world directories reside
+        """
         self.agent_path = os.path.join(resource_path, AGENT_DIRECTORY)
         tools.mkdir(self.agent_path)
-        agent_file_list = []
         self.agents = {}
 
         for user_directory_name, user_directory_names, agent_file_names in os.walk(self.agent_path):
             for agent_file_name in agent_file_names:
-                agent = Nodenet(agent_file_name)
-                if agent:
-                    self.agents[agent.name]= agent
+                agent = nodenet.NodenetManager(agent_file_name)
+                if agent.name: self.agents[agent.name] = None
+
+        self.world_path = os.path.join(resource_path, AGENT_DIRECTORY)
+        tools.mkdir(self.world_path)
+        self.worlds = {}
+
+        for user_directory_name, user_directory_names, world_file_names in os.walk(self.world_path):
+            for world_file_name in world_file_names:
+                world = environment.WorldManager(world_file_name)
+                if world.name: self.worlds[world.name] = None
+
 
 
 

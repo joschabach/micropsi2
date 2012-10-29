4104aba941e2082eb6ba84a1749fb41a91b7f599
Joscha Bach <joscha.bach@gmail.com>
2012-10-29 18:37:43 +0100

    Tests stubs added

---
+++ micropsi_server/tests/test_userManager.py
@@ -0,0 +1,60 @@
+#!/usr/local/bin/python
+# -*- coding: utf-8 -*-
+
+"""
+
+"""
+from unittest import TestCase
+from micropsi_server import usermanagement
+
+__author__ = 'joscha'
+__date__ = '29.10.12'
+
+class TestUserManager(TestCase):
+    def test_create_user(self):
+        self.fail()
+
+    def test_save_users(self):
+        self.fail()
+
+    def test_list_users(self):
+        self.fail()
+
+    def test_set_user_id(self):
+        self.fail()
+
+    def test_set_user_role(self):
+        self.fail()
+
+    def test_set_user_password(self):
+        self.fail()
+
+    def test_delete_user(self):
+        self.fail()
+
+    def test_start_session(self):
+        self.fail()
+
+    def test_switch_user_for_session_token(self):
+        self.fail()
+
+    def test_test_password(self):
+        self.fail()
+
+    def test_end_session(self):
+        self.fail()
+
+    def test_end_all_sessions(self):
+        self.fail()
+
+    def test_refresh_session(self):
+        self.fail()
+
+    def test_check_for_expired_user_sessions(self):
+        self.fail()
+
+    def test_get_permissions_for_session_token(self):
+        self.fail()
+
+    def test_get_user_id_for_session_token(self):
+        self.fail()

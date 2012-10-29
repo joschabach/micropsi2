ac1dba345faa635b84bed7ab477bc6f670a4619f
Joscha Bach <joscha.bach@gmail.com>
2012-10-29 18:27:52 +0100

    Test directory

---
+++ micropsi_core/tests/test_tools.py
@@ -0,0 +1,19 @@
+#!/usr/local/bin/python
+# -*- coding: utf-8 -*-
+
+"""
+
+"""
+from unittest import TestCase
+import micropsi_core.tools
+
+__author__ = 'joscha'
+__date__ = '29.10.12'
+
+
+def test_generate_uid():
+    u1 = micropsi_core.tools.generate_uid()
+    u2 = micropsi_core.tools.generate_uid()
+    assert len(u1)
+    assert len(u2)
+    assert u1 != u2

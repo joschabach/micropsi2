52d6d000d75732ecbf0ecf8d36596771e6a8add7
Joscha Bach <joscha.bach@gmail.com>
2012-10-29 18:37:43 +0100

    Tests stubs added

---
+++ micropsi_core/tests/test_runtime.py
@@ -0,0 +1,239 @@
+#!/usr/local/bin/python
+# -*- coding: utf-8 -*-
+
+"""
+
+"""
+from unittest import TestCase
+
+__author__ = 'joscha'
+__date__ = '29.10.12'
+
+class TestMicroPsiRuntime(TestCase):
+    def test_init_runners(self):
+        self.fail()
+
+    def test_nodenetrunner(self):
+        self.fail()
+
+    def test_worldrunner(self):
+        self.fail()
+
+    def test__get_world_uid_for_nodenet_uid(self):
+        self.fail()
+
+    def test__get_world_class(self):
+        self.fail()
+
+    def test_get_available_nodenets(self):
+        self.fail()
+
+    def test_load_nodenet(self):
+        self.fail()
+
+    def test_unload_nodenet(self):
+        self.fail()
+
+    def test_get_nodenet_area(self):
+        self.fail()
+
+    def test_new_nodenet(self):
+        self.fail()
+
+    def test_delete_nodenet(self):
+        self.fail()
+
+    def test_set_nodenet_properties(self):
+        self.fail()
+
+    def test_start_nodenetrunner(self):
+        self.fail()
+
+    def test_set_nodenetrunner_timestep(self):
+        self.fail()
+
+    def test_get_nodenetrunner_timestep(self):
+        self.fail()
+
+    def test_get_is_nodenet_running(self):
+        self.fail()
+
+    def test_stop_nodenetrunner(self):
+        self.fail()
+
+    def test_step_nodenet(self):
+        self.fail()
+
+    def test_revert_nodenet(self):
+        self.fail()
+
+    def test_save_nodenet(self):
+        self.fail()
+
+    def test_export_nodenet(self):
+        self.fail()
+
+    def test_import_nodenet(self):
+        self.fail()
+
+    def test_merge_nodenet(self):
+        self.fail()
+
+    def test_get_available_worlds(self):
+        self.fail()
+
+    def test_get_world_properties(self):
+        self.fail()
+
+    def test_get_worldadapters(self):
+        self.fail()
+
+    def test_get_world_objects(self):
+        self.fail()
+
+    def test_new_world(self):
+        self.fail()
+
+    def test_delete_world(self):
+        self.fail()
+
+    def test_get_world_view(self):
+        self.fail()
+
+    def test_set_world_properties(self):
+        self.fail()
+
+    def test_start_worldrunner(self):
+        self.fail()
+
+    def test_get_worldrunner_timestep(self):
+        self.fail()
+
+    def test_get_is_world_running(self):
+        self.fail()
+
+    def test_set_worldrunner_timestep(self):
+        self.fail()
+
+    def test_stop_worldrunner(self):
+        self.fail()
+
+    def test_step_world(self):
+        self.fail()
+
+    def test_revert_world(self):
+        self.fail()
+
+    def test_save_world(self):
+        self.fail()
+
+    def test_export_world(self):
+        self.fail()
+
+    def test_import_world(self):
+        self.fail()
+
+    def test_add_gate_monitor(self):
+        self.fail()
+
+    def test_add_slot_monitor(self):
+        self.fail()
+
+    def test_remove_monitor(self):
+        self.fail()
+
+    def test_clear_monitor(self):
+        self.fail()
+
+    def test_export_monitor_data(self):
+        self.fail()
+
+    def test_get_monitor_data(self):
+        self.fail()
+
+    def test_get_nodespace(self):
+        self.fail()
+
+    def test_get_node(self):
+        self.fail()
+
+    def test_add_node(self):
+        self.fail()
+
+    def test_set_node_position(self):
+        self.fail()
+
+    def test_set_node_name(self):
+        self.fail()
+
+    def test_set_node_state(self):
+        self.fail()
+
+    def test_set_node_activation(self):
+        self.fail()
+
+    def test_delete_node(self):
+        self.fail()
+
+    def test_get_available_node_types(self):
+        self.fail()
+
+    def test_get_available_native_module_types(self):
+        self.fail()
+
+    def test_get_nodefunction(self):
+        self.fail()
+
+    def test_set_nodefunction(self):
+        self.fail()
+
+    def test_set_node_parameters(self):
+        self.fail()
+
+    def test_add_node_type(self):
+        self.fail()
+
+    def test_delete_node_type(self):
+        self.fail()
+
+    def test_get_slot_types(self):
+        self.fail()
+
+    def test_get_gate_types(self):
+        self.fail()
+
+    def test_get_gate_function(self):
+        self.fail()
+
+    def test_set_gate_function(self):
+        self.fail()
+
+    def test_set_gate_parameters(self):
+        self.fail()
+
+    def test_get_available_datasources(self):
+        self.fail()
+
+    def test_get_available_datatargets(self):
+        self.fail()
+
+    def test_bind_datasource_to_sensor(self):
+        self.fail()
+
+    def test_bind_datatarget_to_actor(self):
+        self.fail()
+
+    def test_add_link(self):
+        self.fail()
+
+    def test_set_link_weight(self):
+        self.fail()
+
+    def test_get_link(self):
+        self.fail()
+
+    def test_delete_link(self):
+        self.fail()
+
+    def test_align_nodes(self):
+        self.fail()

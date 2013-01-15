be36bc96b65ce2c0504fb64a9b75f08f0c31d934
Tom Lazar <tomster@pyfidelity.com>
2013-01-15 16:37:42 +0100

    add a version string

--- setup.py
+++ setup.py
@@ -1,8 +1,10 @@
 from setuptools import setup
 
+version = '0.1-dev'
 
 setup(
     name='psi_cortex',
+    version=version,
     packages=['micropsi_core', 'micropsi_server'],
     include_package_data=True,
     zip_safe=False,

52839acfba91d3cd7f8bfc7a3862927c3dad43b9
Dominik Welland <hallo@doik.de>
2013-02-05 14:11:42 +0100

    pep8

--- micropsi_core/_runtime_api_world.py
+++ micropsi_core/_runtime_api_world.py
@@ -15,6 +15,7 @@ __date__ = '11.12.12'
 
 import runtime
 
+
 # World
 def get_available_worlds(owner=None):
     """Returns a dict of uids: World of (running and stored) worlds.
@@ -26,6 +27,7 @@ def get_available_worlds(owner=None):
         return dict((uid, runtime.worlds[uid]) for uid in runtime.worlds if runtime.worlds[uid].owner == owner)
     else:
         return runtime.worlds
+
 
 def get_world_properties(world_uid):
     """ Returns some information about the current world for the client:
@@ -44,6 +46,7 @@ def get_world_properties(world_uid):
     data['worldadapters'] = get_worldadapters(world_uid)
     return data
 
+
 def get_worldadapters(world_uid):
     """Returns the world adapters available in the given world"""
 
@@ -55,17 +58,21 @@ def get_worldadapters(world_uid):
         }
     return data
 
+
 def get_world_objects(world_uid, type=None):
     if world_uid in runtime.worlds:
         return runtime.worlds[world_uid].get_world_objects(type)
     return False
 
+
 def add_worldobject(world_uid, type, position, orientation=0.0, name="", parameters=None, uid=None):
     return runtime.worlds[world_uid].add_object(type, position, orientation=orientation, name=name,
         parameters=parameters, uid=uid)
 
+
 def set_worldobject_properties(world_uid, uid, type, position, orientation, name, parameters):
     return runtime.worlds[world_uid].set_object_properties(uid, type, position, orientation, name, parameters)
+
 
 def new_world(world_name, world_type, owner=""):
     """Creates a new world  and registers it.
@@ -80,7 +87,7 @@ def new_world(world_name, world_type, owner=""):
         None if failure
     """
     uid = tools.generate_uid()
-    filename = os.path.join(runtime.DATA_PATH, runtime.WORLD_DIRECTORY, uid+".json")
+    filename = os.path.join(runtime.DATA_PATH, runtime.WORLD_DIRECTORY, uid + ".json")
     runtime.world_data[uid] = Bunch(uid=uid, name=world_name, world_type=world_type, filename=filename, version=1,
         owner=owner)
     with open(filename, 'w+') as fp:
@@ -93,6 +100,7 @@ def new_world(world_name, world_type, owner=""):
         return False, "World type unknown"
     return True, uid
 
+
 def delete_world(world_uid):
     """Removes the world with the given uid from the server (and unloads it from memory if it is running.)"""
     for uid in runtime.nodenets:
@@ -103,38 +111,46 @@ def delete_world(world_uid):
     del runtime.world_data[world_uid]
     return True
 
+
 def get_world_view(world_uid, step):
     """Returns the current state of the world for UI purposes, if current step is newer than the supplied one."""
     if step < runtime.worlds[world_uid].current_step:
         return runtime.worlds[world_uid].get_world_view(step)
     return {}
 
+
 def set_world_properties(world_uid, world_name=None, world_type=None, owner=None):
     """Sets the supplied parameters (and only those) for the world with the given uid."""
     pass
+
 
 def start_worldrunner(world_uid):
     """Starts a thread that regularly advances the world simulation."""
     runtime.worlds[world_uid].is_active = True
     return True
 
+
 def get_worldrunner_timestep():
     """Returns the speed that has been configured for the world runner (in ms)."""
     return runtime.configs['worldrunner_timestep']
 
+
 def get_is_world_running(world_uid):
     """Returns True if an worldrunner is active for the given world, False otherwise."""
     return runtime.worlds[world_uid].is_active
+
 
 def set_worldrunner_timestep(timestep):
     """Sets the interval of the simulation steps for the world runner (in ms)."""
     runtime.configs['worldrunner_timestep'] = timestep
     return True
 
+
 def stop_worldrunner(world_uid):
     """Ends the thread of the continuous world simulation."""
     runtime.worlds[world_uid].is_active = False
     return True
+
 
 def step_world(world_uid, return_world_view=False):
     """Advances the world simulation by one step.
@@ -148,11 +164,13 @@ def step_world(world_uid, return_world_view=False):
         return get_world_view(world_uid)
     return True
 
+
 def revert_world(world_uid):
     """Reverts the world to the last saved state."""
     data = runtime.world_data[world_uid]
     runtime.worlds[world_uid] = get_world_class_from_name(data.world_type)(**data)
     return True
+
 
 def save_world(world_uid):
     """Stores the world state on the server."""
@@ -161,9 +179,11 @@ def save_world(world_uid):
     fp.close()
     return True
 
+
 def export_world(world_uid):
     """Returns a JSON string with the current state of the world."""
     return json.dumps(runtime.worlds[world_uid].data, sort_keys=True, indent=4)
+
 
 def import_world(worlddata, owner=None):
     """Imports a JSON string with world data. May overwrite an existing world."""
@@ -182,14 +202,15 @@ def import_world(worlddata, owner=None):
         **runtime.world_data[data['uid']])
     return data['uid']
 
+
 def get_world_class_from_name(world_type):
     """Returns the class from a world type, if it is known"""
     from world.world import World
     worldclasses = { cls.__name__: cls for cls in vars()['World'].__subclasses__() }
     return worldclasses.get(world_type, World)
 
+
 def get_available_world_types():
     """Returns the names of the available world types"""
     from world.world import World
-    return [ cls.__name__ for cls in vars()['World'].__subclasses__() ]
-
+    return [cls.__name__ for cls in vars()['World'].__subclasses__()]

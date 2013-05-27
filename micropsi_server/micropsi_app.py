baaf0a86cba9706e4e95625e6abecfac69933305
Dominik Welland <hallo@doik.de>
2013-05-27 15:00:19 +0200

    pep8, pyflakes

--- micropsi_server/micropsi_app.py
+++ micropsi_server/micropsi_app.py
@@ -24,7 +24,7 @@ import os
 import json
 import inspect
 import minidoc
-from configuration import RESOURCE_PATH, DEFAULT_HOST, DEFAULT_PORT, VERSION, APPTITLE
+from configuration import DEFAULT_HOST, DEFAULT_PORT, VERSION, APPTITLE
 
 APP_PATH = os.path.dirname(__file__)
 
@@ -461,10 +461,6 @@ def save_all_nodenets():
         response.set_cookie('notification', '{"msg":"All nodenets saved", "status":"success"}', path='/')
         redirect('/nodenet_mgt')
     return template("error", msg="Insufficient rights to access nodenet console")
-
-
-
-
 
 
 @route("/nodenet/import")

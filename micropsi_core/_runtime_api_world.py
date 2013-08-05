c2cec02e5ad1935841b69276178ef66afd5bb4c0
Dominik Welland <hallo@doik.de>
2013-08-05 15:14:47 +0200

    fix circular import problems

--- micropsi_core/_runtime_api_world.py
+++ micropsi_core/_runtime_api_world.py
@@ -6,6 +6,7 @@ Runtime API: operations for creating and maintaining worlds (environments for no
 
 import json
 import os
+import micropsi_core
 from micropsi_core import tools
 from micropsi_core.tools import Bunch
 from micropsi_core.world import world
@@ -22,9 +23,9 @@ def get_available_worlds(owner=None):
         owner (optional): when submitted, the list is filtered by this owner
     """
     if owner:
-        return dict((uid, runtime.worlds[uid]) for uid in runtime.worlds if runtime.worlds[uid].owner == owner)
+        return dict((uid, micropsi_core.runtime.worlds[uid]) for uid in micropsi_core.runtime.worlds if micropsi_core.runtime.worlds[uid].owner == owner)
     else:
-        return runtime.worlds
+        return micropsi_core.runtime.worlds
 
 
 def get_world_properties(world_uid):
@@ -40,7 +41,7 @@ def get_world_properties(world_uid):
         dictionary containing the information
     """
 
-    data = runtime.worlds[world_uid].data
+    data = micropsi_core.runtime.worlds[world_uid].data
     data['worldadapters'] = get_worldadapters(world_uid)
     return data
 
@@ -49,8 +50,8 @@ def get_worldadapters(world_uid):
     """Returns the world adapters available in the given world"""
 
     data = {}
-    if world_uid in runtime.worlds:
-        for name, worldadapter in runtime.worlds[world_uid].supported_worldadapters.items():
+    if world_uid in micropsi_core.runtime.worlds:
+        for name, worldadapter in micropsi_core.runtime.worlds[world_uid].supported_worldadapters.items():
             data[name] = {
                 'datasources': worldadapter.datasources.keys(),
                 'datatargets': worldadapter.datatargets.keys()
@@ -59,22 +60,22 @@ def get_worldadapters(world_uid):
 
 
 def get_world_objects(world_uid, type=None):
-    if world_uid in runtime.worlds:
-        return runtime.worlds[world_uid].get_world_objects(type)
+    if world_uid in micropsi_core.runtime.worlds:
+        return micropsi_core.runtime.worlds[world_uid].get_world_objects(type)
     return False
 
 
 def add_worldobject(world_uid, type, position, orientation=0.0, name="", parameters=None, uid=None):
-    return runtime.worlds[world_uid].add_object(type, position, orientation=orientation, name=name,
+    return micropsi_core.runtime.worlds[world_uid].add_object(type, position, orientation=orientation, name=name,
         parameters=parameters, uid=uid)
 
 
 def set_worldobject_properties(world_uid, uid, type=None, position=None, orientation=None, name=None, parameters=None):
-    return runtime.worlds[world_uid].set_object_properties(uid, type, position, orientation, name, parameters)
+    return micropsi_core.runtime.worlds[world_uid].set_object_properties(uid, type, position, orientation, name, parameters)
 
 
 def set_worldagent_properties(world_uid, uid, position=None, orientation=None, name=None, parameters=None):
-    return runtime.worlds[world_uid].set_agent_properties(uid, position, orientation, name, parameters)
+    return micropsi_core.runtime.worlds[world_uid].set_agent_properties(uid, position, orientation, name, parameters)
 
 
 def new_world(world_name, world_type, owner=""):
@@ -91,15 +92,15 @@ def new_world(world_name, world_type, owner=""):
     """
     uid = tools.generate_uid()
 
-    filename = os.path.join(runtime.RESOURCE_PATH, runtime.WORLD_DIRECTORY, uid + ".json")
-    runtime.world_data[uid] = Bunch(uid=uid, name=world_name, world_type=world_type, filename=filename, version=1,
+    filename = os.path.join(micropsi_core.runtime.RESOURCE_PATH, micropsi_core.runtime.WORLD_DIRECTORY, uid + ".json")
+    micropsi_core.runtime.world_data[uid] = Bunch(uid=uid, name=world_name, world_type=world_type, filename=filename, version=1,
         owner=owner)
     with open(filename, 'w+') as fp:
-        fp.write(json.dumps(runtime.world_data[uid], sort_keys=True, indent=4))
+        fp.write(json.dumps(micropsi_core.runtime.world_data[uid], sort_keys=True, indent=4))
     fp.close()
     try:
-        kwargs = runtime.world_data[uid]
-        runtime.worlds[uid] = get_world_class_from_name(world_type)(**kwargs)
+        kwargs = micropsi_core.runtime.world_data[uid]
+        micropsi_core.runtime.worlds[uid] = get_world_class_from_name(world_type)(**kwargs)
     except AttributeError:
         return False, "World type unknown"
     return True, uid
@@ -107,19 +108,19 @@ def new_world(world_name, world_type, owner=""):
 
 def delete_world(world_uid):
     """Removes the world with the given uid from the server (and unloads it from memory if it is running.)"""
-    for uid in runtime.nodenets:
-        if runtime.nodenets[uid].world and runtime.nodenets[uid].world.uid == world_uid:
-            runtime.nodenets[uid].world = None
-    del runtime.worlds[world_uid]
-    os.remove(runtime.world_data[world_uid].filename)
-    del runtime.world_data[world_uid]
+    for uid in micropsi_core.runtime.nodenets:
+        if micropsi_core.runtime.nodenets[uid].world and micropsi_core.runtime.nodenets[uid].world.uid == world_uid:
+            micropsi_core.runtime.nodenets[uid].world = None
+    del micropsi_core.runtime.worlds[world_uid]
+    os.remove(micropsi_core.runtime.world_data[world_uid].filename)
+    del micropsi_core.runtime.world_data[world_uid]
     return True
 
 
 def get_world_view(world_uid, step):
     """Returns the current state of the world for UI purposes, if current step is newer than the supplied one."""
-    if step < runtime.worlds[world_uid].current_step:
-        return runtime.worlds[world_uid].get_world_view(step)
+    if step < micropsi_core.runtime.worlds[world_uid].current_step:
+        return micropsi_core.runtime.worlds[world_uid].get_world_view(step)
     return {}
 
 
@@ -130,29 +131,29 @@ def set_world_properties(world_uid, world_name=None, world_type=None, owner=None
 
 def start_worldrunner(world_uid):
     """Starts a thread that regularly advances the world simulation."""
-    runtime.worlds[world_uid].is_active = True
+    micropsi_core.runtime.worlds[world_uid].is_active = True
     return True
 
 
 def get_worldrunner_timestep():
     """Returns the speed that has been configured for the world runner (in ms)."""
-    return runtime.configs['worldrunner_timestep']
+    return micropsi_core.runtime.configs['worldrunner_timestep']
 
 
 def get_is_world_running(world_uid):
     """Returns True if an worldrunner is active for the given world, False otherwise."""
-    return runtime.worlds[world_uid].is_active
+    return micropsi_core.runtime.worlds[world_uid].is_active
 
 
 def set_worldrunner_timestep(timestep):
     """Sets the interval of the simulation steps for the world runner (in ms)."""
-    runtime.configs['worldrunner_timestep'] = timestep
+    micropsi_core.runtime.configs['worldrunner_timestep'] = timestep
     return True
 
 
 def stop_worldrunner(world_uid):
     """Ends the thread of the continuous world simulation."""
-    runtime.worlds[world_uid].is_active = False
+    micropsi_core.runtime.worlds[world_uid].is_active = False
     return True
 
 
@@ -163,7 +164,7 @@ def step_world(world_uid, return_world_view=False):
         world_uid: the uid of the simulation world
         return_world_view: if True, return the current world state for UI purposes
     """
-    runtime.worlds[world_uid].step()
+    micropsi_core.runtime.worlds[world_uid].step()
     if return_world_view:
         return get_world_view(world_uid)
     return True
@@ -171,22 +172,22 @@ def step_world(world_uid, return_world_view=False):
 
 def revert_world(world_uid):
     """Reverts the world to the last saved state."""
-    data = runtime.world_data[world_uid]
-    runtime.worlds[world_uid] = get_world_class_from_name(data.world_type)(**data)
+    data = micropsi_core.runtime.world_data[world_uid]
+    micropsi_core.runtime.worlds[world_uid] = get_world_class_from_name(data.world_type)(**data)
     return True
 
 
 def save_world(world_uid):
     """Stores the world state on the server."""
-    with open(os.path.join(runtime.RESOURCE_PATH, runtime.WORLD_DIRECTORY, world_uid) + '.json', 'w+') as fp:
-        fp.write(json.dumps(runtime.worlds[world_uid].data, sort_keys=True, indent=4))
+    with open(os.path.join(micropsi_core.runtime.RESOURCE_PATH, micropsi_core.runtime.WORLD_DIRECTORY, world_uid) + '.json', 'w+') as fp:
+        fp.write(json.dumps(micropsi_core.runtime.worlds[world_uid].data, sort_keys=True, indent=4))
     fp.close()
     return True
 
 
 def export_world(world_uid):
     """Returns a JSON string with the current state of the world."""
-    return json.dumps(runtime.worlds[world_uid].data, sort_keys=True, indent=4)
+    return json.dumps(micropsi_core.runtime.worlds[world_uid].data, sort_keys=True, indent=4)
 
 
 def import_world(worlddata, owner=None):
@@ -196,14 +197,14 @@ def import_world(worlddata, owner=None):
         data['uid'] = tools.generate_uid()
     if owner is not None:
         data['owner'] = owner
-    filename = os.path.join(runtime.RESOURCE_PATH, runtime.WORLD_DIRECTORY, data['uid'])
+    filename = os.path.join(micropsi_core.runtime.RESOURCE_PATH, micropsi_core.runtime.WORLD_DIRECTORY, data['uid'])
     data['filename'] = filename
     with open(filename, 'w+') as fp:
         fp.write(json.dumps(data))
     fp.close()
-    runtime.world_data[data['uid']] = runtime.parse_definition(data, filename)
-    runtime.worlds[data['uid']] = get_world_class_from_name(runtime.world_data[data['uid']].world_type)(
-        **runtime.world_data[data['uid']])
+    micropsi_core.runtime.world_data[data['uid']] = micropsi_core.runtime.parse_definition(data, filename)
+    micropsi_core.runtime.worlds[data['uid']] = get_world_class_from_name(micropsi_core.runtime.world_data[data['uid']].world_type)(
+        **micropsi_core.runtime.world_data[data['uid']])
     return data['uid']
 
 

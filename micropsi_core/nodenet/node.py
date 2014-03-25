1a82e5e8a579481c58e589ed37c17f271cdc9730
Ronnie Vuine <wintermut@tessierashpool.net>
2014-03-25 14:01:57 +0100

    node net stepping should not reset activation to 0, but use a method with proper "reset all slots" semantics "activation" is a property that is defined as sum(slots) when reading and set('gen', activation) when setting. I don't like it.

--- micropsi_core/nodenet/node.py
+++ micropsi_core/nodenet/node.py
@@ -173,6 +173,12 @@ class Node(NetEntity):
         self.data['gate_parameters'][gate_type] = parameters
         self.gates[gate_type].parameters = parameters
 
+    def reset_slots(self):
+        for slot in self.slots.keys():
+            self.slots[slot].activation = 0
+        if self.uid in self.nodenet.active_nodes:
+            del self.nodenet.active_nodes[self.uid]
+        self.data['activation'] = self.activation
 
 class Gate(object):  # todo: take care of gate functions at the level of nodespaces, handle gate params
     """The activation outlet of a node. Nodes may have many gates, from which links originate.

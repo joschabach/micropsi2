2df8f702d37549fbc53fa03dbde337ee5d086dfc
Ronnie Vuine <wintermut@tessierashpool.net>
2014-04-20 21:03:29 +0200

    Pipes making use of the new facility

--- micropsi_core/nodenet/nodefunctions.py
+++ micropsi_core/nodenet/nodefunctions.py
@@ -18,42 +18,42 @@ def concept(nodenet, node=None, **params):
     for type, gate in node.gates.items():
         gate.gate_function(node.get_slot("gen").activation)
 
-def pipe(nodenet, node=None, **params):
+def pipe(nodenet, node=None, sheaf="default", **params):
     gen = 0.0
     por = 0.0
     ret = 0.0
     sub = 0.0
     sur = 0.0
 
-    gen += node.get_slot("sur").voted_activation
+    gen += node.get_slot("sur").get_voted_activation(sheaf)
     if gen < 0: gen = 0
     if gen > 1: gen = 1
 
-    sub += node.get_slot("gen").activation
-    sub += node.get_slot("sur").activation
-    sub += node.get_slot("sub").activation
-    sub += node.get_slot("por").activation
+    sub += node.get_slot("gen").get_activation(sheaf)
+    sub += node.get_slot("sur").get_activation(sheaf)
+    sub += node.get_slot("sub").get_activation(sheaf)
+    sub += node.get_slot("por").get_activation(sheaf)
     if sub > 0: sub = 1
 
-    sur += node.get_slot("sur").voted_activation
+    sur += node.get_slot("sur").get_voted_activation(sheaf)
     if sur < 0: sur = 0
 
-    por += node.get_slot("sur").voted_activation * \
-           (1+node.get_slot("por").activation)
-    por += node.get_slot("por").activation * \
-           (1+node.get_slot("ret").activation)
+    por += node.get_slot("sur").get_voted_activation(sheaf) * \
+           (1+node.get_slot("por").get_activation(sheaf))
+    por += node.get_slot("por").get_activation(sheaf) * \
+           (1+node.get_slot("ret").get_activation(sheaf))
     if por < 1: por = -1
     if por > 1: por = 1
 
-    ret += node.get_slot("ret").activation
+    ret += node.get_slot("ret").get_activation(sheaf)
     if ret == 0: ret = -1
 
     node.activation = gen
-    node.get_gate("gen").gate_function(gen)
-    node.get_gate("por").gate_function(por)
-    node.get_gate("ret").gate_function(ret)
-    node.get_gate("sub").gate_function(sub)
-    node.get_gate("sur").gate_function(sur)
+    node.get_gate("gen").gate_function(gen, sheaf)
+    node.get_gate("por").gate_function(por, sheaf)
+    node.get_gate("ret").gate_function(ret, sheaf)
+    node.get_gate("sub").gate_function(sub, sheaf)
+    node.get_gate("sur").gate_function(sur, sheaf)
 
 def label(nodenet, node, **params):
     node.activation = sum([node.slots[slot].activation for slot in node.slots])

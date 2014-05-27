a2d39f020d180a289b9fb5d1f2ba4ade00b7ef8a
Ronnie Vuine <wintermut@tessierashpool.net>
2014-05-27 17:44:57 +0200

    Actual micropsi_logging.py implementation

--- micropsi_server/micropsi_logging.py
+++ micropsi_server/micropsi_logging.py
@@ -6,13 +6,16 @@
 __author__ = 'rvuine'
 
 import logging
+import time
+
+MAX_RECORDS_PER_STORAGE = 1000
 
 
 class RecordWebStorageHandler(logging.Handler):
 
     record_storage = None
 
-    def __init__(self, record_storage=None):
+    def __init__(self, record_storage):
         """
         Initialize the handler
         """
@@ -25,8 +28,58 @@ class RecordWebStorageHandler(logging.Handler):
         """
 
     def emit(self, record):
-        # add record to self.record_storage
-        pass
+        self.format(record)
+        while len(self.record_storage) >= MAX_RECORDS_PER_STORAGE:
+            del self.record_storage[0]
+        dictrecord = {
+            "logger": record.name,
+            "time": int(round(record.created * 1000)),
+            "level": record.levelname,
+            "msg": record.message
+        }
+        self.record_storage.append(dictrecord)
+
+nodenet_record_storage = []
+world_record_storage = []
+system_record_storage = []
+
+logging.getLogger("nodenet").setLevel(logging.DEBUG)
+logging.getLogger("world").setLevel(logging.DEBUG)
+logging.getLogger("system").setLevel(logging.DEBUG)
+
+logging.getLogger("nodenet").addHandler(RecordWebStorageHandler(nodenet_record_storage))
+logging.getLogger("world").addHandler(RecordWebStorageHandler(world_record_storage))
+logging.getLogger("system").addHandler(RecordWebStorageHandler(system_record_storage))
+
+logging.getLogger("nodenet").debug("Nodenet logger ready.")
+logging.getLogger("world").debug("World logger ready.")
+logging.getLogger("world").debug("System logger ready.")
 
 
-logging.getLogger("nodenet").addHandler(new RecordWebStorageHandler())
+def get_logs(logger="*", after=0):
+    """
+        Returns a dict with the current time and a list of log entries,
+        filtered by logger name and timestamp
+    """
+    logs = []
+    if logger == "*":
+        logs.extend(nodenet_record_storage)
+        logs.extend(world_record_storage)
+        logs.extend(system_record_storage)
+    elif logger == "nodenet":
+        logs.extend(nodenet_record_storage)
+    elif logger == "world":
+        logs.extend( world_record_storage)
+    elif logger == "system":
+        logs.extend(system_record_storage)
+
+    i = len(logs)-1
+    while i >= 0:
+        if logs[i]["time"] < after:
+            del logs[i]
+        i -= 1
+
+    now = int(round(time.time() * 1000))
+    return {
+        "servertime": now,
+        "logs": logs}

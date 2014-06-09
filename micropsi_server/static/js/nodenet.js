joschabach/micropsi2

commit ff055c5f5ddc227e6027a19ee0c61c329df32203

fix partial loading of nodenet

micropsi_server/static/js/nodenet.js
@@ -345,6 +345,7 @@ function setNodespaceData(data){
 }
 
 function refreshNodespace(nodespace, coordinates, step, callback){
+    loaded_coordinates = coordinates;
     method = "get_nodespace";
     nodespace = nodespace || currentNodeSpace;
     params = {
@@ -400,7 +401,7 @@ function refreshViewPortData(){
         refreshNodespace(currentNodeSpace, {
             x:[Math.max(0, left - width), left + 2*width],
             y:[Math.max(0, top-height), top + 2*height]
-        });
+        }, currentSimulationStep - 1);
     }
 }
 

commit 45e1efe2b851608497911bb5b52b12b4dbbbfa93

fix coordinates-error introduced in `ff055c5f`

micropsi_server/static/js/nodenet.js
@@ -347,7 +347,8 @@ function setNodespaceData(data){
 }
 
 function refreshNodespace(nodespace, coordinates, step, callback){
-    loaded_coordinates = coordinates;
+    if(coordinates)
+        loaded_coordinates = coordinates;
     method = "get_nodespace";
     nodespace = nodespace || currentNodeSpace;
     params = {
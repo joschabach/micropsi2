c998351feb80fca2fb38ca4461584ec8a2d631f3
Jonas <jonas.kemper.biz@gmail.com>
2014-06-09 14:36:34 -0400

    bot can now jump

--- micropsi_core/world/minecraft/psidispatcher.py
+++ micropsi_core/world/minecraft/psidispatcher.py
@@ -3,6 +3,8 @@ from spock.mcp.mcpacket import Packet
 __author__ = 'jonas'
 
 STANCE_ADDITION = 1.620
+STEP_LENGTH = 0.2
+JUMPING_MAGIC_NUMBER = 2
 
 class PsiDispatcher():
     
@@ -17,59 +19,57 @@ class PsiDispatcher():
         bot_block = (self.micropsiplugin.clientinfo.position['x'], self.micropsiplugin.clientinfo.position['y'], self.micropsiplugin.clientinfo.position['z'])
         current_column = self.micropsiplugin.world.map.columns[(x_chunk, z_chunk)]
         current_section = current_column.chunks[int((bot_block[1]) // 16)]
+
         if self.micropsiplugin.move_z < 0:
-            current_block = current_section.get(int((self.micropsiplugin.clientinfo.position['x'] - 1)  // 1 % 16),
-                                                              int(self.micropsiplugin.clientinfo.position['y'] // 1 % 16),
-                                                              int(self.micropsiplugin.clientinfo.position['z'] // 1 % 16)).id
-            if current_block == 0:
-                self.micropsiplugin.move(position = {
-                    'x': (self.micropsiplugin.clientinfo.position['x'] + 0.1),
-                    'y': self.micropsiplugin.clientinfo.position['y'],
-                    'z': self.micropsiplugin.clientinfo.position['z'],
-                    'yaw': self.micropsiplugin.clientinfo.position['yaw'],
-                    'pitch': self.micropsiplugin.clientinfo.position['pitch'],
-                    'on_ground': self.micropsiplugin.clientinfo.position['on_ground'],
-                    'stance': self.micropsiplugin.clientinfo.position['y'] + STANCE_ADDITION
-                    })
+            target_coords = ((self.micropsiplugin.clientinfo.position['x'] + STEP_LENGTH),
+                              self.micropsiplugin.clientinfo.position['y'],
+                              self.micropsiplugin.clientinfo.position['z'])
+            self.move(target_coords, current_section, "z+")
         if self.micropsiplugin.move_z > 0:
-            current_block = current_section.get(int((self.micropsiplugin.clientinfo.position['x'] + 1)  // 1 % 16),
-                                                              int(self.micropsiplugin.clientinfo.position['y'] // 1 % 16),
-                                                              int(self.micropsiplugin.clientinfo.position['z'] // 1 % 16)).id
-            if current_block == 0:
-                self.micropsiplugin.move(position = {
-                    'x': (self.micropsiplugin.clientinfo.position['x'] - 0.1),
-                    'y': self.micropsiplugin.clientinfo.position['y'],
-                    'z': self.micropsiplugin.clientinfo.position['z'],
-                    'yaw': self.micropsiplugin.clientinfo.position['yaw'],
-                    'pitch': self.micropsiplugin.clientinfo.position['pitch'],
-                    'on_ground': self.micropsiplugin.clientinfo.position['on_ground'],
-                    'stance': self.micropsiplugin.clientinfo.position['y'] + STANCE_ADDITION
-                    })
+            target_coords = ((self.micropsiplugin.clientinfo.position['x'] - STEP_LENGTH),
+                              self.micropsiplugin.clientinfo.position['y'],
+                              self.micropsiplugin.clientinfo.position['z'])
+            self.move(target_coords, current_section, "z-")
         if self.micropsiplugin.move_x < 0:
-            current_block = current_section.get(int(self.micropsiplugin.clientinfo.position['x']  // 1 % 16),
-                                                              int(self.micropsiplugin.clientinfo.position['y'] // 1 % 16),
-                                                              int((self.micropsiplugin.clientinfo.position['z'] - 1) // 1 % 16)).id
-            if current_block == 0:
-                self.micropsiplugin.move(position = {
-                    'x': (self.micropsiplugin.clientinfo.position['x']),
-                    'y': self.micropsiplugin.clientinfo.position['y'],
-                    'z': self.micropsiplugin.clientinfo.position['z'] + 0.1,
-                    'yaw': self.micropsiplugin.clientinfo.position['yaw'],
-                    'pitch': self.micropsiplugin.clientinfo.position['pitch'],
-                    'on_ground': self.micropsiplugin.clientinfo.position['on_ground'],
-                    'stance': self.micropsiplugin.clientinfo.position['y'] + STANCE_ADDITION
-                    })
+            target_coords = ((self.micropsiplugin.clientinfo.position['x']),
+                              self.micropsiplugin.clientinfo.position['y'],
+                              self.micropsiplugin.clientinfo.position['z'] + STEP_LENGTH)
+            self.move(target_coords, current_section, "x+")
         if self.micropsiplugin.move_x > 0:
-            current_block = current_section.get(int(self.micropsiplugin.clientinfo.position['x']  // 1 % 16),
-                                                              int(self.micropsiplugin.clientinfo.position['y'] // 1 % 16),
-                                                              int((self.micropsiplugin.clientinfo.position['z'] + 1) // 1 % 16)).id
-            if current_block == 0:
-                self.micropsiplugin.move(position = {
-                    'x': (self.micropsiplugin.clientinfo.position['x']),
-                    'y': self.micropsiplugin.clientinfo.position['y'],
-                    'z': self.micropsiplugin.clientinfo.position['z'] - 0.1,
-                    'yaw': self.micropsiplugin.clientinfo.position['yaw'],
-                    'pitch': self.micropsiplugin.clientinfo.position['pitch'],
-                    'on_ground': self.micropsiplugin.clientinfo.position['on_ground'],
-                    'stance': self.micropsiplugin.clientinfo.position['y'] + STANCE_ADDITION
-                    })
+            target_coords = ((self.micropsiplugin.clientinfo.position['x']),
+                              self.micropsiplugin.clientinfo.position['y'],
+                              self.micropsiplugin.clientinfo.position['z'] - STEP_LENGTH)
+            self.move(target_coords, current_section, "x-")
+
+
+    def move(self, target_coords, current_section, direction):
+            if direction == "x+":
+                target_block_coords = (int((1 * JUMPING_MAGIC_NUMBER * STEP_LENGTH + target_coords[0]) // 1 % 16),
+                                       int(target_coords[1] // 1 % 16),
+                                       int(target_coords[2] // 1 % 16))
+            if direction == "x-":
+                target_block_coords = (int((-1 * JUMPING_MAGIC_NUMBER * STEP_LENGTH + target_coords[0]) // 1 % 16),
+                                       int(target_coords[1] // 1 % 16),
+                                       int(target_coords[2] // 1 % 16))
+            if direction == "z+":
+                target_block_coords = (int(target_coords[0] // 1 % 16),
+                                       int(target_coords[1] // 1 % 16),
+                                       int((1 * JUMPING_MAGIC_NUMBER * STEP_LENGTH + target_coords[2]) // 1 % 16))
+            if direction == "z-":
+                target_block_coords = (int(target_coords[0] // 1 % 16),
+                                       int(target_coords[1] // 1 % 16),
+                                       int((-1 * JUMPING_MAGIC_NUMBER * STEP_LENGTH + target_coords[2]) // 1 % 16))
+
+            target_block = current_section.get(*target_block_coords).id
+            jump = 0
+            if target_block != 0:
+                jump = 1
+            self.micropsiplugin.move(position = {
+                'x': target_coords[0],
+                'y': target_coords[1] + jump,
+                'z': target_coords[2],
+                'yaw': self.micropsiplugin.clientinfo.position['yaw'],
+                'pitch': self.micropsiplugin.clientinfo.position['pitch'],
+                'on_ground': self.micropsiplugin.clientinfo.position['on_ground'],
+                'stance': target_coords[1] + jump + STANCE_ADDITION
+                })

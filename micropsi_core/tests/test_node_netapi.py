74b2888ade9bcd480367a6710ce8ae43d1f7a95a
Ronnie Vuine <wintermute@tessierashpool.net>
2014-06-18 16:11:38 -0400

    Testing node in nodespace creation, get_node

--- micropsi_core/tests/test_node_netapi.py
+++ micropsi_core/tests/test_node_netapi.py
@@ -58,7 +58,7 @@ def add_dummyworld(fixed_nodenet):
 
 
 def test_node_netapi_create_register_node(fixed_nodenet):
-    # test gen looping behaviour
+    # test register node creation
     net, netapi, source = prepare(fixed_nodenet)
     node = netapi.create_node("Register", "Root", "TestName")
 
@@ -76,3 +76,78 @@ def test_node_netapi_create_register_node(fixed_nodenet):
     assert node.data['gate_parameters']['gen'] == node.get_gate('gen').parameters
     assert node.data['name'] == node.name
     assert node.data['type'] == node.type
+
+    node = netapi.create_node("Register", "Root")
+    #TODO: teh weirdness, server-internally, we return uids as names, clients don't see this, confusion ensues
+    #assert node.data['name'] == node.name
+
+
+def test_node_netapi_create_concept_node(fixed_nodenet):
+    # test concept node generation
+    net, netapi, source = prepare(fixed_nodenet)
+    node = netapi.create_node("Concept", "Root", "TestName")
+
+    # basic logic tests
+    assert node is not None
+    assert node.parent_nodespace == "Root"
+    assert node.type == "Concept"
+    assert node.uid is not None
+    assert node.nodenet is net
+    assert len(node.get_gate('gen').outgoing) == 0
+    assert len(node.get_gate('gen').sheaves) == 1
+    assert len(node.get_gate('sub').outgoing) == 0
+    assert len(node.get_gate('sub').sheaves) == 1
+    assert len(node.get_gate('sur').outgoing) == 0
+    assert len(node.get_gate('sur').sheaves) == 1
+    assert len(node.get_gate('por').outgoing) == 0
+    assert len(node.get_gate('por').sheaves) == 1
+    assert len(node.get_gate('ret').outgoing) == 0
+    assert len(node.get_gate('ret').sheaves) == 1
+    assert len(node.get_gate('cat').outgoing) == 0
+    assert len(node.get_gate('cat').sheaves) == 1
+    assert len(node.get_gate('exp').outgoing) == 0
+    assert len(node.get_gate('exp').sheaves) == 1
+    assert len(node.get_gate('sym').outgoing) == 0
+    assert len(node.get_gate('sym').sheaves) == 1
+    assert len(node.get_gate('ref').outgoing) == 0
+    assert len(node.get_gate('ref').sheaves) == 1
+
+    # frontend/persistency-oriented data dictionary test
+    assert node.data['uid'] == node.uid
+    assert node.data['gate_parameters']['gen'] == node.get_gate('gen').parameters
+    assert node.data['gate_parameters']['sub'] == node.get_gate('sub').parameters
+    assert node.data['gate_parameters']['sur'] == node.get_gate('sur').parameters
+    assert node.data['gate_parameters']['por'] == node.get_gate('por').parameters
+    assert node.data['gate_parameters']['ret'] == node.get_gate('ret').parameters
+    assert node.data['gate_parameters']['cat'] == node.get_gate('cat').parameters
+    assert node.data['gate_parameters']['exp'] == node.get_gate('exp').parameters
+    assert node.data['gate_parameters']['sym'] == node.get_gate('sym').parameters
+    assert node.data['gate_parameters']['ref'] == node.get_gate('ref').parameters
+    assert node.data['name'] == node.name
+    assert node.data['type'] == node.type
+
+    node = netapi.create_node("Concept", "Root")
+    #TODO: teh weirdness, server-internally, we return uids as names, clients don't see this, confusion ensues
+    #assert node.data['name'] == node.name
+
+
+def test_node_netapi_create_node_in_nodespace(fixed_nodenet):
+    # test register node creation
+    net, netapi, source = prepare(fixed_nodenet)
+    nodespace = netapi.create_node("Nodespace", "Root", "NestedNodespace")
+    node = netapi.create_node("Register", nodespace.uid, "TestName")
+
+    assert node.parent_nodespace == nodespace.uid
+    assert node.data['parent_nodespace'] == nodespace.uid
+
+
+def test_node_netapi_get_node(fixed_nodenet):
+    # test register node creation
+    net, netapi, source = prepare(fixed_nodenet)
+    node = netapi.create_node("Register", "Root", "TestName")
+
+    queried_node = netapi.get_node(node.uid)
+    assert queried_node.uid == node.uid
+    assert queried_node.name == node.name
+    assert queried_node.data == node.data
+    assert queried_node.type == node.type

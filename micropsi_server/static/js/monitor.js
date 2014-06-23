0d1d260ff7ec5238812a9fbafabcb1278ffd0a27
Dominik Welland <Doik@users.noreply.github.com>
2014-06-23 19:00:36 -0400

    only scroll logs if container scrolled to bottom

--- micropsi_server/static/js/monitor.js
+++ micropsi_server/static/js/monitor.js
@@ -121,12 +121,18 @@ $(function(){
 
     function setLoggingData(data){
         var html = log_container.html();
+        var height = log_container.height();
+        var scrollHeight = log_container[0].scrollHeight;
+        var st = log_container.scrollTop();
+        var doscroll = (st >= (scrollHeight - height));
         last_logger_call = data.logs.servertime;
         for(var idx in data.logs.logs){
             html += '<span class="logentry log_'+data.logs.logs[idx].level+'">'+data.logs.logs[idx].logger+' | ' + data.logs.logs[idx].msg +'</span>'
         }
         log_container.html(html);
-        log_container.scrollTop(log_container[0].scrollHeight);
+        if(doscroll){
+            log_container.scrollTop(log_container[0].scrollHeight);
+        }
     }
 
     function bindEvents(){

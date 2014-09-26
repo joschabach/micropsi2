35471230f6e69fadfa395e9b7aaea3288d489426
Priska Herger <priska@23bit.net>
2014-09-26 20:09:42 +0200

    adds activation setter to Gate.

--- micropsi_core/nodenet/node.py
+++ micropsi_core/nodenet/node.py
@@ -309,6 +309,10 @@ class Gate(object):  # todo: take care of gate functions at the level of nodespa
     def activation(self):
         return self.sheaves['default'].activation
 
+    @activation.setter
+    def activation(self, activation):
+        self.sheaves['default'].activation = activation
+
     def __init__(self, type, node, sheaves=None, gate_function=None, parameters=None, gate_defaults=None):
         """create a gate.
 

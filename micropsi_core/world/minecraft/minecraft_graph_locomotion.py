ae8090972d83c5c1bda170eee3834666b700facf
Ronnie Vuine <wintermute@tessierashpool.net>
2014-10-22 12:57:56 +0200

    Adding minecraft locations and connections

--- micropsi_core/world/minecraft/minecraft_graph_locomotion.py
+++ micropsi_core/world/minecraft/minecraft_graph_locomotion.py
@@ -20,16 +20,122 @@ class MinecraftGraphLocomotion(WorldAdapter):
         'exit_three_uid': None,
     }
 
-    uid = tools.generate_uid()
-    loco_nodes[uid] = loco_node_template.copy()
-    loco_nodes[uid]['uid'] = uid
-    loco_nodes[uid]['name'] = "home"
-    loco_nodes[uid]['x'] = -105
-    loco_nodes[uid]['y'] = 65
-    loco_nodes[uid]['z'] = 59
-    loco_nodes[uid]['exit_one_uid'] = uid
+    home_uid = tools.generate_uid()
+    underground_garden_uid = tools.generate_uid()
+    village_uid = tools.generate_uid()
+    cathedral_uid = tools.generate_uid()
+    summit_uid = tools.generate_uid()
+    cloud_uid = tools.generate_uid()
+    bungalow_uid = tools.generate_uid()
+    farm_uid = tools.generate_uid()
+    forest_uid = tools.generate_uid()
+    desert_outpost_uid = tools.generate_uid()
+    swamp_uid = tools.generate_uid()
 
-    current_loco_node = loco_nodes[uid]
+    loco_nodes[home_uid] = loco_node_template.copy()
+    loco_nodes[home_uid]['name'] = "home"
+    loco_nodes[home_uid]['uid'] = home_uid
+    loco_nodes[home_uid]['x'] = -105
+    loco_nodes[home_uid]['y'] = 65
+    loco_nodes[home_uid]['z'] = 59
+    loco_nodes[home_uid]['exit_one_uid'] = cloud_uid
+    loco_nodes[home_uid]['exit_two_uid'] = cathedral_uid
+    loco_nodes[cloud_uid]['exit_three_uid'] = village_uid
+
+    # assuming we start at the home position
+    current_loco_node = loco_nodes[home_uid]
+
+    loco_nodes[underground_garden_uid] = loco_node_template.copy()
+    loco_nodes[underground_garden_uid]['name'] = "underground garden"
+    loco_nodes[underground_garden_uid]['uid'] = underground_garden_uid
+    loco_nodes[underground_garden_uid]['x'] = -264
+    loco_nodes[underground_garden_uid]['y'] = 65
+    loco_nodes[underground_garden_uid]['z'] = 65
+    loco_nodes[underground_garden_uid]['exit_one_uid'] = home_uid
+    loco_nodes[underground_garden_uid]['exit_two_uid'] = village_uid
+
+    loco_nodes[village_uid] = loco_node_template.copy()
+    loco_nodes[village_uid]['name'] = "village"
+    loco_nodes[village_uid]['uid'] = village_uid
+    loco_nodes[village_uid]['x'] = -293
+    loco_nodes[village_uid]['y'] = 65
+    loco_nodes[village_uid]['z'] = -220
+    loco_nodes[village_uid]['exit_one_uid'] = underground_garden_uid
+    loco_nodes[village_uid]['exit_two_uid'] = home_uid
+
+    loco_nodes[cathedral_uid] = loco_node_template.copy()
+    loco_nodes[cathedral_uid]['name'] = "cathedral"
+    loco_nodes[cathedral_uid]['uid'] = cathedral_uid
+    loco_nodes[cathedral_uid]['x'] = -100
+    loco_nodes[cathedral_uid]['y'] = 65
+    loco_nodes[cathedral_uid]['z'] = 282
+    loco_nodes[cathedral_uid]['exit_one_uid'] = home_uid
+    loco_nodes[cathedral_uid]['exit_two_uid'] = cloud_uid
+    loco_nodes[cathedral_uid]['exit_three_uid'] = bungalow_uid
+
+    loco_nodes[summit_uid] = loco_node_template.copy()
+    loco_nodes[summit_uid]['name'] = "summit"
+    loco_nodes[summit_uid]['uid'] = summit_uid
+    loco_nodes[summit_uid]['x'] = -233
+    loco_nodes[summit_uid]['y'] = 102
+    loco_nodes[summit_uid]['z'] = 307
+    loco_nodes[forest_uid]['exit_one_uid'] = swamp_uid
+
+    loco_nodes[cloud_uid] = loco_node_template.copy()
+    loco_nodes[cloud_uid]['name'] = "cloud"
+    loco_nodes[cloud_uid]['uid'] = cloud_uid
+    loco_nodes[cloud_uid]['x'] = -98
+    loco_nodes[cloud_uid]['y'] = 65
+    loco_nodes[cloud_uid]['z'] = 198
+    loco_nodes[cloud_uid]['exit_one_uid'] = home_uid
+    loco_nodes[cloud_uid]['exit_two_uid'] = cathedral_uid
+
+    loco_nodes[bungalow_uid] = loco_node_template.copy()
+    loco_nodes[bungalow_uid]['name'] = "bungalow"
+    loco_nodes[bungalow_uid]['uid'] = bungalow_uid
+    loco_nodes[bungalow_uid]['x'] = 28
+    loco_nodes[bungalow_uid]['y'] = 65
+    loco_nodes[bungalow_uid]['z'] = 292
+    loco_nodes[bungalow_uid]['exit_one_uid'] = cathedral_uid
+    loco_nodes[bungalow_uid]['exit_two_uid'] = farm_uid
+
+    loco_nodes[farm_uid] = loco_node_template.copy()
+    loco_nodes[farm_uid]['name'] = "farm"
+    loco_nodes[farm_uid]['uid'] = farm_uid
+    loco_nodes[farm_uid]['x'] = -50
+    loco_nodes[farm_uid]['y'] = 65
+    loco_nodes[farm_uid]['z'] = 410
+    loco_nodes[farm_uid]['exit_one_uid'] = bungalow_uid
+    loco_nodes[farm_uid]['exit_two_uid'] = cathedral_uid
+    loco_nodes[farm_uid]['exit_three_uid'] = forest_uid
+
+    loco_nodes[forest_uid] = loco_node_template.copy()
+    loco_nodes[forest_uid]['name'] = "forest"
+    loco_nodes[forest_uid]['uid'] = forest_uid
+    loco_nodes[forest_uid]['x'] = -273
+    loco_nodes[forest_uid]['y'] = 65
+    loco_nodes[forest_uid]['z'] = 782
+    loco_nodes[forest_uid]['exit_one_uid'] = farm_uid
+    loco_nodes[forest_uid]['exit_two_uid'] = desert_outpost_uid
+    loco_nodes[forest_uid]['exit_three_uid'] = swamp_uid
+
+    loco_nodes[desert_outpost_uid] = loco_node_template.copy()
+    loco_nodes[desert_outpost_uid]['name'] = "desert outpost"
+    loco_nodes[desert_outpost_uid]['uid'] = desert_outpost_uid
+    loco_nodes[desert_outpost_uid]['x'] = -243
+    loco_nodes[desert_outpost_uid]['y'] = 65
+    loco_nodes[desert_outpost_uid]['z'] = 958
+    loco_nodes[desert_outpost_uid]['exit_one_uid'] = forest_uid
+
+    loco_nodes[swamp_uid] = loco_node_template.copy()
+    loco_nodes[swamp_uid]['name'] = "swamp"
+    loco_nodes[swamp_uid]['uid'] = swamp_uid
+    loco_nodes[swamp_uid]['x'] = -529
+    loco_nodes[swamp_uid]['y'] = 65
+    loco_nodes[swamp_uid]['z'] = 504
+    loco_nodes[swamp_uid]['exit_one_uid'] = forest_uid
+    loco_nodes[swamp_uid]['exit_two_uid'] = summit_uid
+
 
     def __init__(self, world, uid=None, **data):
         super(MinecraftGraphLocomotion, self).__init__(world, uid, **data)

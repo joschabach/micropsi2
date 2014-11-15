3e161b5d7ea3fbcef261401573aaf6a5463f846e
Ronnie Vuine <wintermute@tessierashpool.net>
2014-11-15 18:00:24 +0100

    Fix for gen-looping lower boundary (avoid activating SUR when GEN is about to hit lower threshold)

--- micropsi_core/nodenet/nodefunctions.py
+++ micropsi_core/nodenet/nodefunctions.py
@@ -106,7 +106,7 @@ def pipe(netapi, node=None, sheaf="default", **params):
     neighbors = len(node.get_slot("por").incoming)
 
     gen += node.get_slot("gen").get_activation(sheaf)
-    if gen < 0.1: gen = 0
+    if gen < 0.1: gen = 0                                                   # cut off gen loop at lower threshold
     gen += node.get_slot("sur").get_activation(sheaf)
     gen += node.get_slot("exp").get_activation(sheaf)
     if gen > 1: gen = 1
@@ -119,7 +119,7 @@ def pipe(netapi, node=None, sheaf="default", **params):
 
     sur += node.get_slot("sur").get_activation(sheaf)
     if sur == 0: sur += node.get_slot("sur").get_activation("default")      # no activation in our sheaf, maybe from sensors?
-    sur += 0 if node.get_slot("gen").get_activation(sheaf) < 0.1 else 1
+    sur += 0 if node.get_slot("gen").get_activation(sheaf) < 0.2 else 1     # cut off sur-reports from gen looping before the loop fades away
     sur += node.get_slot("exp").get_activation(sheaf)
     if sur > 0:     # else: always propagate failure
         sur *= 0 if node.get_slot("por").get_activation(sheaf) < 0 else 1

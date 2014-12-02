joschabach/micropsi2

commit fa35e5502bb976126af8ccf700013b270c251492

Centralizing gate parameter initialization and validation

micropsi_core/nodenet/dict_engine/dict_node.py
@@ -93,6 +93,17 @@ class DictNode(NetEntity, Node):
 
         gate_parameters = self.nodetype.gate_defaults.copy()
         gate_parameters.update(self.__non_default_gate_parameters)
+
+        for key in gate_parameters.copy():
+            if key in self.nodetype.gate_defaults:
+                try:
+                    gate_parameters[key] = float(gate_parameters[key])
+                except:
+                    logging.getLogger('nodenet').warn('Invalid gate parameter value for gate %s, param %s, node %s' % (type, key, self.uid))
+                    gate_parameters[key] = self.nodetype.gate_defaults.get(key, 0)
+            else:
+                gate_parameters[key] = float(gate_parameters[key])
+
         for gate in self.nodetype.gatetypes:
             if gate_activations is None or gate not in gate_activations:
                 sheaves_to_use = None

micropsi_core/nodenet/node.py
@@ -156,6 +156,9 @@ class Node(metaclass=ABCMeta):
         return self.__nodetype
 
     def __init__(self, nodetype_name, nodetype):
+        """
+        Constructor needs the string name of this node's type, and a Nodetype instance
+        """
         self.__nodetype_name = nodetype_name
         self.__nodetype = nodetype
 
@@ -167,11 +170,16 @@ class Node(metaclass=ABCMeta):
         pass
 
     @abstractmethod
-    def get_slot(self, type):
+    def set_gate_parameters(self, gate_type, parameters):
         pass
 
     @abstractmethod
-    def set_gate_parameters(self, gate_type, parameters):
+    def clone_non_default_gate_parameters(self):
+        pass
+
+
+    @abstractmethod
+    def get_slot(self, type):
         pass
 
     @abstractmethod
@@ -211,18 +219,11 @@ class Node(metaclass=ABCMeta):
         pass
 
     @abstractmethod
-    def get_gate_parameters(self):
-        pass
-
-    @abstractmethod
     def node_function(self):
         """
         """
         pass
 
-    @abstractmethod
-    def clone_non_default_gate_parameters(self):
-        pass
 
     def get_gate_types(self):
         """
@@ -299,17 +300,7 @@ class Gate(object):
                 self.sheaves[key] = dict(uid=sheaves[key]['uid'], name=sheaves[key]['name'], activation=sheaves[key]['activation'])
         self.__outgoing = {}
         self.gate_function = gate_function or self.gate_function
-        self.parameters = {}
-        if parameters is not None:
-            for key in parameters:
-                if key in Nodetype.GATE_DEFAULTS:
-                    try:
-                        self.parameters[key] = float(parameters[key])
-                    except:
-                        logging.getLogger('nodenet').warn('Invalid gate parameter value for gate %s, param %s, node %s' % (type, key, node.uid))
-                        self.parameters[key] = Nodetype.GATE_DEFAULTS.get(key, 0)
-                else:
-                    self.parameters[key] = float(parameters[key])
+        self.parameters = parameters
         self.monitor = None
 
     def get_links(self):

micropsi_core/runtime.py
@@ -622,7 +622,7 @@ def _perform_copy_nodes(nodenet, nodes, nodespaces, target_nodespace=None, copy_
             original.name,
             uid,
             deepcopy(original.clone_parameters()),
-            original.get_gate_parameters())
+            original.clone_non_default_gate_parameters())
 
     # copy the links
     links_to_copy = set()

commit 1b23ede7e65014b8676229e6cdf99ff00bffe121

Fixing gate parameter validation broken in fa35e55

micropsi_core/nodenet/dict_engine/dict_node.py
@@ -96,15 +96,17 @@ class DictNode(NetEntity, Node):
         gate_parameters = self.nodetype.gate_defaults.copy()
         gate_parameters.update(self.__non_default_gate_parameters)
 
-        for key in gate_parameters.copy():
-            if key in self.nodetype.gate_defaults:
-                try:
-                    gate_parameters[key] = float(gate_parameters[key])
-                except:
-                    logging.getLogger('nodenet').warn('Invalid gate parameter value for gate %s, param %s, node %s' % (type, key, self.uid))
-                    gate_parameters[key] = self.nodetype.gate_defaults.get(key, 0)
-            else:
-                gate_parameters[key] = float(gate_parameters[key])
+        gate_parameters_for_validation = gate_parameters.copy()
+        for gate_name in gate_parameters_for_validation:
+            for key in gate_parameters_for_validation[gate_name]:
+                if key in self.nodetype.gate_defaults:
+                    try:
+                        gate_parameters[gate_name][key] = float(gate_parameters[gate_name][key])
+                    except:
+                        logging.getLogger('nodenet').warn('Invalid gate parameter value for gate %s, param %s, node %s' % (gate_name, key, self.uid))
+                        gate_parameters[gate_name][key] = self.nodetype.gate_defaults[gate_name].get(key, 0)
+                else:
+                    gate_parameters[gate_name][key] = float(gate_parameters[gate_name][key])
 
         for gate in self.nodetype.gatetypes:
             if gate_activations is None or gate not in gate_activations:
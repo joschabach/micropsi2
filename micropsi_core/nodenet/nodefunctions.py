fc0848c1d284db75b343a5ea8ef929994acb3357
Ronnie Vuine <wintermute@tessierashpool.net>
2015-02-03 09:26:03 +0100

    No link counting allowed

--- micropsi_core/nodenet/nodefunctions.py
+++ micropsi_core/nodenet/nodefunctions.py
@@ -133,7 +133,7 @@ def pipe(netapi, node=None, sheaf="default", **params):
     sur += node.get_slot("exp").get_activation(sheaf)
     if sur > 0 and len(node.get_slot('ret').get_links()) == 1:     # else: always propagate failure
         sur = 0
-    sur *= node.get_slot("por").get_activation(sheaf) if len(node.get_slot("por").get_links()) == 1 else 1
+    sur *= node.get_slot("por").get_activation(sheaf) if not node.get_slot("por").empty else 1
 
     if node.get_slot('por').empty and node.get_slot('ret').empty:           # both empty
         classifierelements = 0
@@ -143,7 +143,7 @@ def pipe(netapi, node=None, sheaf="default", **params):
         sur /= classifierelements if classifierelements > 1 else 1          # classifier case
 
     por += node.get_slot("sur").get_activation(sheaf)
-    por *= node.get_slot("por").get_activation(sheaf) if len(node.get_slot("por").get_links()) == 1 else 1
+    por *= node.get_slot("por").get_activation(sheaf) if not node.get_slot("por").empty else 1
     por += (0 if node.get_slot("gen").get_activation(sheaf) < 0.1 else 1) * \
            (1+node.get_slot("por").get_activation(sheaf))
     por += node.get_slot("por").get_activation(sheaf) if node.get_slot("sub").get_activation(sheaf) == 0 and node.get_slot("sur").get_activation(sheaf) == 0 else 0

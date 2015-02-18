joschabach/micropsi2

commit 2d212c5b78b151e46b21ea4021572ce1db7f3387

prevent nodenet from being instantiated twice

micropsi_core/runtime.py
@@ -60,6 +60,8 @@ logger = MicropsiLogger({
     'nodenet': LOGGING['level_nodenet']
 })
 
+nodenet_lock = threading.Lock()
+
 
 def add_signal_handler(handler):
     signal_handler_registry.append(handler)
@@ -226,9 +228,6 @@ def get_nodenet(nodenet_uid):
 
 def load_nodenet(nodenet_uid):
     """ Load the nodenet with the given uid into memeory
-        TODO: how do we know in which world we want to load the nodenet?
-        I've added the world uid to the nodenet serialized data for the moment
-
         Arguments:
             nodenet_uid
         Returns:
@@ -238,7 +237,10 @@ def load_nodenet(nodenet_uid):
     """
     if nodenet_uid in nodenet_data:
         world = worldadapter = None
+
+        nodenet_lock.acquire()
         if nodenet_uid not in nodenets:
+            nodenets[nodenet_uid] = None
             data = nodenet_data[nodenet_uid]
 
             if data.world:
@@ -256,6 +258,7 @@ def load_nodenet(nodenet_uid):
                     nodetypes=nodetypes, native_modules=native_modules)
             # Add additional engine types here
             else:
+                nodenet_lock.release()
                 return False, "Nodenet %s requires unknown engine %s" % (nodenet_uid, engine)
 
             if "settings" in data:
@@ -267,6 +270,8 @@ def load_nodenet(nodenet_uid):
             worldadapter = nodenets[nodenet_uid].worldadapter
         if world:
             world.register_nodenet(worldadapter, nodenets[nodenet_uid])
+
+        nodenet_lock.release()
         return True, nodenet_uid
     return False, "Nodenet %s not found in %s" % (nodenet_uid, RESOURCE_PATH)
 

commit 34d52563af86fc8f0490e2664328bfccca8c17bc

this was wrongly introduced in `2d212c5`

micropsi_core/runtime.py
@@ -240,7 +240,6 @@ def load_nodenet(nodenet_uid):
 
         nodenet_lock.acquire()
         if nodenet_uid not in nodenets:
-            nodenets[nodenet_uid] = None
             data = nodenet_data[nodenet_uid]
 
             if data.world:
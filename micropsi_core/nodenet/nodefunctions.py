e252b71e5d6fbffb923596c2f10f9df1c7b95bfd
Dominik Welland <hallo@doik.de>
2015-03-17 11:02:32 +0100

    ops, variable initialization...

--- micropsi_core/nodenet/nodefunctions.py
+++ micropsi_core/nodenet/nodefunctions.py
@@ -237,12 +237,13 @@ def trigger(netapi, node=None, sheaf="default", **params):
     else:
         waitfrom = node.get_state("waitfrom") or -1
 
+        timeout = node.get_parameter("timeout") or 10        # todo: use emo_resolution
+        condition = node.get_parameter("condition") or "="
+        response = node.get_parameter("response")
+        if response is None:
+            response = 1
+
         if node.get_slot('sub').activation > 0:
-            timeout = node.get_parameter("timeout") or 10        # todo: use emo_resolution
-            condition = node.get_parameter("condition") or "="
-            response = node.get_parameter("response")
-            if response is None:
-                response = 1
             currentstep = netapi.step
 
             success = (condition == "=" and node.get_slot('sur').activation == int(response)) or \

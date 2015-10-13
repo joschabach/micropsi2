Correct weights array calculation when saving

micropsi_core/nodenet/theano_engine/theano_partition.py
@@ -769,15 +769,17 @@ class TheanoPartition():
 
         inlink_from_element_count = 0
         inlink_to_element_count = 0
+        weight_count = 0
         for spid, inlinks in self.inlinks.items():
             inlink_from_element_count += len(inlinks[0].get_value(borrow=True))
             inlink_to_element_count += len(inlinks[1].get_value(borrow=True))
+            weight_count += len(inlinks[0].get_value(borrow=True)) * len(inlinks[1].get_value(borrow=True))
         inlinks_pids = np.zeros(len(self.inlinks), dtype=np.int16)
         inlink_from_lengths = np.zeros(len(self.inlinks), dtype=np.int32)
         inlink_to_lengths = np.zeros(len(self.inlinks), dtype=np.int32)
         inlink_from_elements = np.zeros(inlink_from_element_count, dtype=np.int32)
         inlink_to_elements = np.zeros(inlink_to_element_count, dtype=np.int32)
-        inlink_weights = np.zeros(inlink_from_element_count*inlink_to_element_count, dtype=self.nodenet.numpyfloatX)
+        inlink_weights = np.zeros(weight_count, dtype=self.nodenet.numpyfloatX)
 
         from_offset = 0
         to_offset = 0
@@ -792,7 +794,7 @@ class TheanoPartition():
             inlink_to_lengths[i] = to_length
             inlink_from_elements[from_offset:from_offset+from_length] = from_elements
             inlink_to_elements[to_offset:to_offset+to_length] = to_elements
-            inlink_weights[(from_offset*to_offset):(from_offset*to_offset)+((from_offset+from_length)*(to_offset+to_length))] = np.ravel(weights)
+            inlink_weights[(from_offset*to_offset):(from_offset*to_offset)+(from_length*to_length)] = np.ravel(weights)
             from_offset += from_length
             to_offset += to_length
 
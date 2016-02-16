3b555a8be45e4a62ef8fb3f3af40765ec8a2daa9
Ronnie Vuine <wintermute@tessierashpool.net>
2016-02-16 15:55:05 +0100

    TimeseriesWorld now using configurable worlds facilities

--- micropsi_core/world/timeseries/timeseries.py
+++ micropsi_core/world/timeseries/timeseries.py
@@ -1,7 +1,7 @@
 """
 Worlds and bodies for agents whose habitats are ordered sequences of vectors.
 """
-import os.path
+import os
 from configuration import config as cfg
 from micropsi_core.world.world import World
 from micropsi_core.world.worldadapter import WorldAdapter, ArrayWorldAdapter
@@ -24,22 +24,34 @@ class TimeSeries(World):
 
     def __init__(self, filename, world_type="Island", name="", owner="", engine=None, uid=None, version=1, config={}):
         World.__init__(self, filename, world_type=world_type, name=name, owner=owner, uid=uid, version=version, config=config)
-        path = os.path.join(cfg['micropsi2']['data_directory'], 'timeseries.npz')
-        print("loading timeseries from", path, "for world", uid)
 
-        with np.load(path) as f:
-            self.timeseries = f['data']
-            self.ids = f['ids']
-            self.startdate = f['startdate']
-            self.enddate = f['enddate']
+        filename = config.get('time_series_data_file', "timeseries.npz")
+        if os.path.isabs(filename):
+            path = filename
+        else:
+            path = os.path.join(cfg['micropsi2']['data_directory'], filename)
+        self.logger.info("loading timeseries from %s for world %s" % (path, uid))
+
+        try:
+            with np.load(path) as f:
+                self.timeseries = f['data']
+                self.ids = f['ids']
+                self.startdate = f['startdate']
+                self.enddate = f['enddate']
+        except IOError as error:
+            self.logger.error("Could not load data file %s, error was: %s" % (path, str(error)))
+            return
 
         # todo use the new configurable world options.
-        dummydata = False # present the same random pattern in each step.
-        z_transform = True  # for each ID, center on mean & normalize by standard deviation
-        clip_and_scale = False # for each ID, center on mean & clip to 4 standard deviations and rescale to [0,1].
-        sigmoid = True # for each ID, z-transform and apply a sigmoid activation function
-        self.shuffle = True  # randomize order of presentation
-        assert(not (clip_and_scale and sigmoid))
+        dummydata = config('dummydata') == "True"
+        z_transform = config('z_transform') == "True"
+        clip_and_scale = config('clip_and_scale') == "True"
+        sigmoid = config('sigmoid') == "True"
+        self.shuffle = config('shuffle') == "True"
+
+        if clip_and_scale and sigmoid:
+            self.logger.warn("clip_and_scale and sigmoid cannot both be configured, choosing sigmoid")
+            clip_and_scale = False
 
         def sigm(X):
             """ sigmoid that avoids float overflows for very small inputs.
@@ -48,7 +60,6 @@ class TimeSeries(World):
             cutoff = np.log(np.finfo(X.dtype).max) - 1
             X[np.nan_to_num(X) <= -cutoff] = -cutoff
             return 1. / (1. + np.exp(-X))
-
 
         if (z_transform or clip_and_scale or sigmoid) and not dummydata:
             data_z = np.empty_like(self.timeseries)
@@ -70,7 +81,7 @@ class TimeSeries(World):
             self.timeseries = data_z if not sigmoid else sigm(data_z)
 
         if dummydata:
-            print("! Using dummy data")
+            self.logger.warn("! Using dummy data")
             n_ids = self.timeseries.shape[0]
             self.timeseries = np.tile(np.random.rand(n_ids,1),(1,10))
 
@@ -88,6 +99,43 @@ class TimeSeries(World):
             t = self.permutation[t]
         return self.timeseries[:, t]
 
+    @staticmethod
+    def get_config_options():
+        """ Returns a list of configuration-options for this world.
+        Expected format:
+        [{
+            'name': 'param1',
+            'description': 'this is just an example',
+            'options': ['value1', 'value2'],
+            'default': 'value1'
+        }]
+        description, options and default are optional settings
+        """
+        return [
+            {'name': 'time_series_data_file',
+             'description': 'The data file with the time series',
+             'default': 'timeseries.npz'},
+            {'name': 'shuffle',
+             'description': 'Randomize order of presentation',
+             'default': 'True',
+             'options': ["True", "False"]},
+            {'name': 'z_transform',
+             'description': 'For each ID, center on mean & normalize by standard deviation',
+             'default': 'True',
+             'options': ["True", "False"]},
+            {'name': 'clip_and_scale',
+             'description': 'For each ID, center on mean & clip to 4 standard deviations and rescale to [0,1].',
+             'default': 'False',
+             'options': ["True", "False"]},
+            {'name': 'sigmoid',
+             'description': 'For each ID, z-transform and apply a sigmoid activation function',
+             'default': 'True',
+             'options': ["True", "False"]},
+            {'name': 'dummy_data',
+             'description': 'Present the same random pattern in each step (instead of the actual time series data)',
+             'default': 'False',
+             'options': ["True", "False"]}
+        ]
 
 class TimeSeriesRunner(ArrayWorldAdapter):
 

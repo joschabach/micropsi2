9ab2e6a3a9187708d58473ff50cade668bd716c9
Dominik Welland <hallo@doik.de>
2016-03-03 14:36:20 +0100

    up the coverage a bit :)

--- micropsi_core/tests/test_operations.py
+++ micropsi_core/tests/test_operations.py
@@ -33,13 +33,14 @@ def test_autoalign_operation(test_nodenet):
     assert ops['autoalign']['parameters'] == []
 
     api = runtime.nodenets[test_nodenet].netapi
+    ns1 = api.create_nodespace(None, "foo")
     p1 = api.create_node("Pipe", None, "p1")
     p2 = api.create_node("Pipe", None, "p2")
     p3 = api.create_node("Pipe", None, "p3")
     api.link_with_reciprocal(p1, p2, 'subsur')
     api.link_with_reciprocal(p1, p3, 'subsur')
     api.link_with_reciprocal(p2, p3, 'porret')
-    runtime.run_operation(test_nodenet, "autoalign", {}, [p1.uid, p2.uid, p3.uid])
+    runtime.run_operation(test_nodenet, "autoalign", {}, [p1.uid, p2.uid, p3.uid, ns1])
     assert p1.position[0] == p2.position[0]
     assert p1.position[1] < p2.position[1]
     assert p2.position[1] == p3.position[1]

3447d0c980d05159db39fa369a8c6854f1c02478
Dominik Welland <hallo@doik.de>
2016-03-31 18:39:44 +0200

    first shot at netapi: frontend

---
+++ micropsi_server/static/js/netapi_console.js
@@ -0,0 +1,97 @@
+
+
+// thanks to Chris Coyier at css-tricks.com
+jQuery.fn.putCursorAtEnd = function() {
+    return this.each(function() {
+        $(this).focus()
+        // If this function exists...
+        if (this.setSelectionRange) {
+            // ... then use it (Doesn't work in IE)
+            // Double the length because Opera is inconsistent about whether a carriage return is one character or two. Sigh.
+            var len = $(this).val().length * 2;
+            this.setSelectionRange(len, len);
+        } else {
+            // ... otherwise replace the contents with itself
+            // (Doesn't work in Google Chrome)
+            $(this).val($(this).val());
+        }
+        // Scroll to the bottom, in case we're in a tall textarea
+        // (Necessary for Firefox and Google Chrome)
+        this.scrollTop = 999999;
+    });
+};
+
+$(function(){
+
+    var input = $('#console_input');
+    var currentNodenet = '';
+    var cookieval = $.cookie('selected_nodenet');
+    if (cookieval && cookieval.indexOf('/')){
+        currentNodenet = cookieval.split('/')[0];
+    }
+
+    var history = $('#console_history');
+    var container = $('#netapi_console .code_container');
+
+    hljs.highlightBlock(history[0]);
+
+    command_history = [];
+    history_pointer = -1;
+
+    input.keydown(function(event){
+        var code = input.val();
+        if(event.keyCode == 13){
+            api.call('run_netapi_command', {nodenet_uid: currentNodenet, command: code}, function(data){
+                var hist = history.text();
+                hist += "\n" + ">>> " + code;
+                if(data){
+                    hist += "\n" + data;
+                }
+                history.text(hist);
+                input.val('');
+                hljs.highlightBlock(history[0]);
+                container.scrollTop(999999999)
+                command_history.push(code);
+                $(document).trigger('runner_stepped');
+            }, function(error){
+                var hist = history.text();
+                if(error.data){
+                    hist += "\n" + ">>> " + code;
+                    hist += '\nERROR: '+error.data;
+                }
+                history.text(hist);
+                input.val('');
+                hljs.highlightBlock(history[0]);
+                container.scrollTop(999999999)
+                command_history.push(code);
+            });
+        }
+        if(event.keyCode == 38){
+            // arrow up
+            if(code == '' && history_pointer == -1){
+                event.preventDefault();
+                history_pointer = command_history.length - 1;
+                input.val(command_history[history_pointer])
+                input.putCursorAtEnd()
+            } else if(history_pointer > 0 && code == command_history[history_pointer]) {
+                event.preventDefault();
+                history_pointer -= 1;
+                input.val(command_history[history_pointer])
+                input.putCursorAtEnd()
+            }
+        } else if(event.keyCode == 40){
+            // arrow down
+            if(history_pointer < command_history.length - 1 && code == command_history[history_pointer]) {
+                event.preventDefault();
+                history_pointer += 1;
+                input.val(command_history[history_pointer])
+                input.putCursorAtEnd()
+            }
+        }
+        else {
+            history_pointer = -1
+        }
+    })
+
+});
+
---
+++ micropsi_server/view/netapi_console.tpl
@@ -0,0 +1,22 @@
+
+<link rel="stylesheet" href="/static/css/highlight_styles/github.css">
+<script src="/static/js/highlight.pack.js"></script>
+<script src="/static/js/netapi_console.js"></script>
+
+<div class="sectionbar">
+    <span data-toggle="collapse" data-target="#netapi_console"><i
+        class="icon-chevron-right"></i>
+        Netapi Console
+        </span>
+</div>
+
+
+<div id="netapi_console" class="section-margin frontend_section collapse in monospace">
+    <div class="section multiple">
+            <pre class="code_container"><code class="python" id="console_history"># enter your code here.
+# eg: netapi.create_node("Pipe", None, "example")
+</code></pre>
+>>><input type="text" id="console_input" />
+
+    </div>
+</div>

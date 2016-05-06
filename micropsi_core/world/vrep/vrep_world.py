6d681016130ded1fa35f6f6dffe135422b376ee5
Ronnie Vuine <wintermute@tessierashpool.net>
2016-05-06 12:05:23 +0200

    Generalizing towards other robots MTB_Robot should work

--- micropsi_core/world/vrep/vrep_world.py
+++ micropsi_core/world/vrep/vrep_world.py
@@ -4,7 +4,6 @@ import logging
 import numpy as np
 import matplotlib
 matplotlib.use('agg')
-import matplotlib.image as mpimg
 import matplotlib.pyplot as plt
 
 from io import BytesIO
@@ -19,20 +18,22 @@ from micropsi_core.world.worldadapter import ArrayWorldAdapter
 class VREPWorld(World):
     """ A vrep robot simulator environment
         In V-REP, the following setup has to be performed:
-        - An LBR_iiwa_7_R800 has to have been added to the scene
         - simExtRemoteApiStart(19999) has to have been run
         - the simulation must have been started
     """
+    supported_worldadapters = ['Robot']
 
     assets = {
         'template': 'vrep/vrep.tpl',
         'js': "vrep/vrep.js",
     }
 
-    supported_worldadapters = ['iiwa']
-
     def __init__(self, filename, world_type="VREPWorld", name="", owner="", engine=None, uid=None, version=1, config={}):
         World.__init__(self, filename, world_type=world_type, name=name, owner=owner, uid=uid, version=version)
+
+        self.robot_name = config['robot_name']
+        self.vision_type = config['vision_type']
+        self.control_type = config['control_type']
 
         vrep.simxFinish(-1)  # just in case, close all opened connections
         self.clientID = vrep.simxStart('127.0.0.1', 19999, True, 0, 5000, 5)  # Connect to V-REP
@@ -46,15 +47,14 @@ class VREPWorld(World):
 
         self.logger.info('Ping time to v-rep: %dms' % pingtime)
 
-        res, self.iiwa_handle = vrep.simxGetObjectHandle(self.clientID, "LBR_iiwa_7_R800", vrep.simx_opmode_blocking)
+        res, self.iiwa_handle = vrep.simxGetObjectHandle(self.clientID, self.robot_name, vrep.simx_opmode_blocking)
         self.handle_res(res)
         if self.iiwa_handle < 1:
-            raise Exception("There seems to be no robot with the name LBR_iiwa_7_R800 in the v-rep simulation.")
+            raise Exception("There seems to be no robot with the name %s in the v-rep simulation." % self.robot_name)
 
         res, self.joints = vrep.simxGetObjects(self.clientID, vrep.sim_object_joint_type, vrep.simx_opmode_blocking)
         self.handle_res(res)
-        if len(self.joints) != 7:
-            raise Exception("Could not get handles for all 7 joints of the LBR_iiwa_7_R800.")
+        self.logger.info("Found robot with %d joints" % len(self.joints))
 
         res, self.ball_handle = vrep.simxGetObjectHandle(self.clientID, "Ball", vrep.simx_opmode_blocking)
         self.handle_res(res)
@@ -64,26 +64,27 @@ class VREPWorld(World):
             res, _ = vrep.simxGetObjectPosition(self.clientID, self.ball_handle, -1, vrep.simx_opmode_streaming)
             if res != 0 and res != 1:
                 self.handle_res(res)
-            res, _ = vrep.simxGetObjectPosition(self.clientID, self.joints[6], -1, vrep.simx_opmode_streaming)
+            res, _ = vrep.simxGetObjectPosition(self.clientID, self.joints[len(self.joints)-1], -1, vrep.simx_opmode_streaming)
             if res != 0 and res != 1:
                 self.handle_res(res)
 
-        res, self.observer_handle = vrep.simxGetObjectHandle(self.clientID, "Observer", vrep.simx_opmode_blocking)
-        self.handle_res(res)
-        if self.observer_handle < 1:
-            self.logger.warn("Could not get handle for Observer vision sensor, vision will not be available.")
-        else:
-            res, resolution, image = vrep.simxGetVisionSensorImage(self.clientID, self.observer_handle, 0, vrep.simx_opmode_streaming) # _split+4000)
-            if res != 0 and res != 1:
-                self.handle_res(res)
+        if self.vision_type == "grayscale":
+            res, self.observer_handle = vrep.simxGetObjectHandle(self.clientID, "Observer", vrep.simx_opmode_blocking)
+            self.handle_res(res)
+            if self.observer_handle < 1:
+                self.logger.warn("Could not get handle for Observer vision sensor, vision will not be available.")
             else:
-                time.sleep(1)
-                res, resolution, image = vrep.simxGetVisionSensorImage(self.clientID, self.observer_handle, 0, vrep.simx_opmode_buffer)
-                self.vision_resolution = resolution
-                if len(resolution) != 2:
-                    raise Exception("Could not determine vision resolution after 1 second wait time.")
+                res, resolution, image = vrep.simxGetVisionSensorImage(self.clientID, self.observer_handle, 0, vrep.simx_opmode_streaming) # _split+4000)
+                if res != 0 and res != 1:
+                    self.handle_res(res)
                 else:
-                    self.logger.info("Vision resolution is %s" % str(self.vision_resolution))
+                    time.sleep(1)
+                    res, resolution, image = vrep.simxGetVisionSensorImage(self.clientID, self.observer_handle, 0, vrep.simx_opmode_buffer)
+                    self.vision_resolution = resolution
+                    if len(resolution) != 2:
+                        raise Exception("Could not determine vision resolution after 1 second wait time.")
+                    else:
+                        self.logger.info("Vision resolution is %s" % str(self.vision_resolution))
 
     def handle_res(self, res):
         if res != vrep.simx_return_ok:
@@ -91,23 +92,42 @@ class VREPWorld(World):
             self.logger.warn("v-rep call returned error code %d, error: %s" % (res, error))
 
     def get_world_view(self, step):
-        plots = {}
-        for uid in self.agents:
-            image = self.agents[uid].image
-            if image:
-                bio = BytesIO()
-                image.figure.savefig(bio, format="png")
-                plots[uid] = base64.encodebytes(bio.getvalue()).decode("utf-8")
+        if self.vision_type == "grayscale":
+            plots = {}
+            for uid in self.agents:
+                image = self.agents[uid].image
+                if image:
+                    bio = BytesIO()
+                    image.figure.savefig(bio, format="png")
+                    plots[uid] = base64.encodebytes(bio.getvalue()).decode("utf-8")
 
-        return {
-            'objects': self.get_world_objects(),
-            'agents': self.data.get('agents', {}),
-            'current_step': self.current_step,
-            'plots': plots
-        }
+            return {
+                'objects': self.get_world_objects(),
+                'agents': self.data.get('agents', {}),
+                'current_step': self.current_step,
+                'plots': plots
+            }
+        else:
+            return None
+
+    @staticmethod
+    def get_config_options():
+        return [
+            {'name': 'robot_name',
+             'description': 'The name of the robot object in V-REP',
+             'default': 'LBR_iiwa_7_R800'},
+            {'name': 'control_type',
+             'description': 'The type of input sent to the robot',
+             'default': 'force/torque',
+             'options': ["force/torque", "angles"]},
+            {'name': 'vision_type',
+             'description': 'Type of vision information to receive',
+             'default': 'none',
+             'options': ["none", "grayscale"]}
+        ]
 
 
-class iiwa(ArrayWorldAdapter):
+class Robot(ArrayWorldAdapter):
 
     def __init__(self, world, uid=None, **data):
         super().__init__(world, uid, **data)
@@ -129,6 +149,8 @@ class iiwa(ArrayWorldAdapter):
         for i in range(len(self.world.joints)):
             self.available_datasources.append("joint_force_%s" % str(i + 1))
 
+        self.current_angle_target_values = np.zeros_like(self.world.joints)
+
         self.restart_offset = 0
         self.execute_offset = 1
         self.joint_offset = 2
@@ -136,17 +158,18 @@ class iiwa(ArrayWorldAdapter):
         self.reward_offset = 0
         self.joint_angle_offset = 1
         self.joint_force_offset = self.joint_angle_offset + len(self.world.joints)
-        self.image_offset = self.joint_force_offset + len(self.world.joints)
-        self.image_length = self.world.vision_resolution[0] * self.world.vision_resolution[1]
 
-        for y in range(self.world.vision_resolution[1]):
-            for x in range(self.world.vision_resolution[0]):
-                self.available_datasources.append("px_%d_%d" % (x, y))
+        if self.world.vision_type == "grayscale":
+            self.image_offset = self.joint_force_offset + len(self.world.joints)
+            self.image_length = self.world.vision_resolution[0] * self.world.vision_resolution[1]
 
-        self.image = plt.imshow(np.zeros(shape=(self.world.vision_resolution[0],self.world.vision_resolution[1])), cmap="bone")
-        self.image.norm.vmin = 0
-        self.image.norm.vmax = 1
-        self.current_angle_target_values = np.zeros_like(self.world.joints)
+            for y in range(self.world.vision_resolution[1]):
+                for x in range(self.world.vision_resolution[0]):
+                    self.available_datasources.append("px_%d_%d" % (x, y))
+
+            self.image = plt.imshow(np.zeros(shape=(self.world.vision_resolution[0],self.world.vision_resolution[1])), cmap="bone")
+            self.image.norm.vmin = 0
+            self.image.norm.vmax = 1
 
     def get_available_datasources(self):
         return self.available_datasources
@@ -174,30 +197,35 @@ class iiwa(ArrayWorldAdapter):
             self.current_angle_target_values = np.array(self.datatarget_values[self.joint_offset:self.joint_offset+len(self.world.joints)])
             vrep.simxPauseCommunication(self.world.clientID, True)
             for i, joint_handle in enumerate(self.world.joints):
-                tval = self.current_angle_target_values[i] * math.pi
-                vrep.simxSetJointTargetPosition(self.world.clientID, joint_handle, tval, vrep.simx_opmode_oneshot)
+                if self.world.control_type == "force/torque":
+                    tval = self.current_angle_target_values[i] * math.pi
+                    vrep.simxSetJointTargetPosition(self.world.clientID, joint_handle, tval, vrep.simx_opmode_oneshot)
+                elif self.world.control_type == "angles":
+                    tval = self.current_angle_target_values[i] * 180
+                    vrep.simxSetJointPosition(self.world.clientID, joint_handle, tval, vrep.simx_opmode_oneshot)
             vrep.simxPauseCommunication(self.world.clientID, False)
 
         # get data and feedback
         # read reward value
         if self.world.ball_handle > 0:
             res, ball_pos = vrep.simxGetObjectPosition(self.world.clientID, self.world.ball_handle, -1, vrep.simx_opmode_buffer)
-            res, joint_pos = vrep.simxGetObjectPosition(self.world.clientID, self.world.joints[6], -1, vrep.simx_opmode_streaming)
+            res, joint_pos = vrep.simxGetObjectPosition(self.world.clientID, self.world.joints[len(self.world.joints)-1], -1, vrep.simx_opmode_streaming)
             dist = np.linalg.norm(np.array(ball_pos) - np.array(joint_pos))
             self.datasource_values[self.reward_offset] = -dist
 
         # read joint angle and force values
         res, joint_ids, something, data, se = vrep.simxGetObjectGroupData(self.world.clientID, vrep.sim_object_joint_type, 15, vrep.simx_opmode_blocking)
-        if len(data) != 14:
-            self.world.logger.warn("Could not get robot state values, skipping update")
-            return
-
         for i, joint_handle in enumerate(self.world.joints):
             target_angle = self.datatarget_values[self.joint_offset + i]
-            angle = data[i*2] / math.pi
-            force = data[i*2 + 1]
-            if abs(angle) - abs(target_angle) < .001 and execute:
-                self.datatarget_feedback_values[self.joint_offset + i] = 1
+            angle = 0
+            force = 0
+            if self.world.control_type == "force/torque":
+                angle = data[i*2] / math.pi
+                force = data[i*2 + 1]
+                if abs(angle) - abs(target_angle) < .001 and execute:
+                    self.datatarget_feedback_values[self.joint_offset + i] = 1
+            elif self.world.control_type == "angles":
+                angle = data[i * 2] / 180
             self.datasource_values[self.joint_angle_offset + i] = angle
             self.datasource_values[self.joint_force_offset + i] = force
 

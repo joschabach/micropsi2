dd133a47cb66ac14354263c87b63a1c767c88670
Ronnie Vuine <wintermute@tessierashpool.net>
2016-05-20 12:02:01 +0200

    VREP world creation fix

--- micropsi_core/world/vrep/vrep_world.py
+++ micropsi_core/world/vrep/vrep_world.py
@@ -42,7 +42,7 @@ class VREPWorld(World):
         self.ball_handle = -1
 
         vrep.simxFinish(-1)  # just in case, close all opened connections
-        self.clientID = vrep.simxStart(config['vrep_host'], config['vrep_port'], True, 0, 5000, 5)  # Connect to V-REP
+        self.clientID = vrep.simxStart(config['vrep_host'], int(config['vrep_port']), True, 0, 5000, 5)  # Connect to V-REP
         if self.clientID == -1:
             self.logger.critical("Could not connect to v-rep")
             return

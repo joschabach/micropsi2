e9497ac522c867fffe209a18ef4ebddab7fd3904
cknd <ck-github@erleuchtet.org>
2016-06-02 14:24:04 +0200

    merged vrep_connection_daemon

--- micropsi_core/world/vrep/vrep_world.py
+++ micropsi_core/world/vrep/vrep_world.py
@@ -478,11 +478,11 @@ class Robot(ArrayWorldAdapter):
 
                 count += 1
                 if count == 10:
-                    self.logger.warning("Robot did not complete movement in time, giving up.");
-                    self.logger.warning("Joint   targets: %s" % str(targets));
-                    self.logger.warning("Joint positions: %s" % str(joint_positions));
+                    self.logger.info("Robot did not complete movement in time, giving up.");
+                    self.logger.info("Joint   targets: %s" % str(targets));
+                    self.logger.info("Joint positions: %s" % str(joint_positions));
                     movement_finished = True
-                time.sleep(0.2)
+                time.sleep(0.1)
                 res, joint_ids, something, data, se = vrep.simxGetObjectGroupData(self.clientID,
                                                                                   vrep.sim_object_joint_type, 15,
                                                                                   vrep.simx_opmode_blocking)

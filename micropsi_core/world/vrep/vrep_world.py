b47cdc4502d8b69acc555d1e0980ef4b9c91c024
Dominik Welland <hallo@doik.de>
2016-08-03 16:37:18 +0200

    explicitly kill vrep before spawning a new process

--- micropsi_core/world/vrep/vrep_world.py
+++ micropsi_core/world/vrep/vrep_world.py
@@ -127,6 +127,12 @@ class VREPWatchdog(threading.Thread):
 
     def spawn_vrep(self):
         notify = self.process is not None
+        if self.process is not None:
+            try:
+                self.process.kill()
+            except:
+                pass
+
         fp = open('/tmp/vrep.log', 'a')
         self.process = subprocess.Popen(self.args, stdout=fp, preexec_fn=preexec_function)
         if notify:
@@ -145,7 +151,10 @@ class VREPWatchdog(threading.Thread):
     def terminate(self):
         self.is_active = False
         self.stop.set()
-        self.process.kill()
+        try:
+            self.process.kill()
+        except:
+            pass
 
 
 class VrepCallMixin():

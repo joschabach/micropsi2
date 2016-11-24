643597f15bfa36adf2eec9bbe1d6ac7ae174d791
cknd <clemens@micropsi-industries.com>
2016-11-24 16:18:54 +0100

    generic OAI wrapper (untested)

--- micropsi_core/world/gym/gym_world.py
+++ micropsi_core/world/gym/gym_world.py
@@ -9,6 +9,39 @@ import gym
 
 "OpenAI Gym interface"
 
+def inspect_space(gym_space):
+    """
+    find the dimensionaltity and (if discrete) number of values of an OAI state/action space
+
+    Params
+    ----
+    gym_space : open ai gym.spaces.Box or gym.spaces.Discrete object
+
+    Returns
+    ----
+    n_dim: int
+        nr of dimensions of the space
+    n_discrete: int or None
+        if the space is discrete: the nr of allowed values.
+        in that case, allowed values are the integers {0,...,n}
+
+    """
+    if isinstance(gym_space, gym.spaces.Box):
+        n_dim = gym_space.shape[0]
+        n_discrete = False
+
+    elif isinstance(gym_space, gym.spaces.Discrete):
+        n_dim = 1
+        n_discrete = gym_space.n
+    else:
+        # some OAI envs have discrete action spaces with
+        # more than 2 action choices, or even multiple dimensions
+        # with some nr of discrete choices in each. so far we don't
+        # deal with that.
+        raise Exception('OAI worldadapter currently requires environments with continuous or 1D discrete state/action spaces')
+    return n_dim, n_discrete
+
+
 class OAIGym(World):
 
     supported_worldadapters = ['OAIGymAdapter']
@@ -16,26 +49,43 @@ class OAIGym(World):
     def __init__(self, filename, world_type="OAIGym", name="", owner="", engine=None, uid=None, version=1, config={}):
         World.__init__(self, filename, world_type=world_type, name=name, owner=owner, uid=uid, version=version)
 
-        self.env = gym.make('CartPole-v0')
+        self.env = gym.make(config['env_id'])
 
-        self.n_dim_state = self.env.observation_space.shape[0]
-        if isinstance(self.env.action_space, gym.spaces.Discrete):
-            # the gym cartpole env happens to have a discrete action space
-            self.n_dim_action = 1
-            self.n_action_options = self.env.action_space.n # this 2 in the cartpole env, meaning integers {0,1} are allowed
-        else:
-            raise NotImplementedError()
+        self.n_dim_state, self.n_discrete_states = inspect_space(self.env.observation_space)
+        self.n_dim_action, self.n_discrete_actions = inspect_space(self.env.observation_space)
+
+    @classmethod
+    def get_config_options(cls):
+        return [
+            {'name': 'env_id',
+             'description': 'OpenAI environment ID'
+             'default': 'Cartpole-v0'},
+        ]
+
+
 
 class OAIGymAdapter(ArrayWorldAdapter):
 
     def __init__(self, world, uid=None, **data):
         super().__init__(world, uid, **data)
 
-        for state_dim in range(self.world.n_dim_state):
-            self.add_datasource("s%d" % state_dim)
+        # 1D discrete state space: one data source for each possible state
+        if self.n_discrete_states:
+            for k in range(self.n_discrete_states):
+                self.add_datasource("s%d" % k)
+        # continuous state space: one data source for each state dimension
+        else:
+            for state_dim in range(self.world.n_dim_state):
+                self.add_datasource("s%d" % state_dim)
 
-        for action_dim in range(self.world.n_dim_action):
-            self.add_datatarget("a%d" % action_dim)
+        # 1D discrete action space: one data target for each possible action
+        if self.n_discrete_actions:
+            for k in range(self.n_discrete_actions):
+                self.add_datatarget("a%d" % k)
+        # continuous action space: one data target for each action dimension
+        else:
+            for action_dim in range(self.world.n_dim_action):
+                self.add_datasource("a%d" % action_dim)
 
         self.add_datatarget("restart")
 
@@ -47,22 +97,36 @@ class OAIGymAdapter(ArrayWorldAdapter):
     def update_data_sources_and_targets(self):
         # print('\nworldadapter.update, datatarget values:\n', self.datatarget_values)
 
-        action = self.datatarget_values[0:self.world.n_dim_action]
-        restart = self.datatarget_values[self.world.n_dim_action]
+        action_values = self.datatarget_values[0:-1] # all datatarget values except 'restart'
+        restart = self.datatarget_values[-1]
 
         if restart > 0:
-            self.reset_simulation_state()
-            return
-
-        if self.world.n_action_options == 2:
-            action = int(action > 0.5) # converts float actions to 0 or 1
+            obs = self.world.env.reset()
         else:
-            raise NotImplementedError()
+            if self.world.n_discrete_actions:
+                # for discrete action spaces, each action is represented by one datatarget, which is
+                # considered active if it's nonzero. the agent needs to make sure only one is active.
+                # OAI expects an integer < n, encoding which of the n available actions to take. so:
+                action = np.where(action_values)
+                if len(action)>1:
+                    raise Exception('Cannot do multiple actions at the same time in a discrete, 1D action space.')
+            else:
+                action = action_values
 
-        obs, r, terminal, info = self.world.env.step(action)
+            obs, r, terminal, info = self.world.env.step(action)
+
         self.world.env.render()
 
-        state = np.concatenate([obs, [r], [int(terminal)]])
+        if self.world.n_discrete_states:
+            # for discrete state spaces, OAI returns its observation as a single integer < n,
+            # indicating which of the n states it is in. we have one data source for each state,
+            # taking values 0 or 1.
+            obs_vector = np.zeros(self.world.n_discrete_states)
+            obs_vector[obs] = 1
+        else:
+            obs_vector = obs
+
+        state = np.concatenate([obs_vector, [r], [int(terminal)]])
         # if terminal:
         #     print('terminal')
         self.datasource_values =  state
@@ -70,11 +134,31 @@ class OAIGymAdapter(ArrayWorldAdapter):
         # print('\nworldadapter.update, datasource values:\n', self.datasource_values)
 
     def reset_simulation_state(self):
-        obs = self.world.env.reset()
-        self.world.env.render()
-        self.datasource_values = np.concatenate([obs, [0], [0]])
-        # print('\nworldadapter did reset, new datasource values:\n', self.datasource_values)
+        raise Exception('uaegh I thought this would never be called if I dont call it')
 
     def reset_datatargets(self):
         """ resets (zeros) the datatargets """
         self.datatarget_values[:] = 0.0
+
+
+if __name__ == '__main__':
+
+    environment_ids = ['CartPole-v0',
+                        'CartPole-v1',
+                        'Acrobot-v1',
+                        'MountainCar-v0',
+                        'MountainCarContinuous-v0',
+                        'Pendulum-v0',
+                        'LunarLander-v2',
+                        'BipedalWalker-v2',
+                        'Copy-v0',
+                        'Reverse-v0',
+                         ]
+
+    for env_id in environment_ids:
+        env = gym.make(env_id)
+        print(env.spec.id)
+        print('obs:, ', env.observation_space)
+        print('action:', env.action_space)
+        print()
+        import ipdb; ipdb.set_trace(context=6)

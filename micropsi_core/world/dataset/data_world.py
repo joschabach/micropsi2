97c94954e984377c9226b6e9c4b5f10d3800579a
cknd <clemens@micropsi-industries.com>
2017-01-18 16:28:32 +0100

    supervised learning worldadapter

--- micropsi_core/world/dataset/data_world.py
+++ micropsi_core/world/dataset/data_world.py
@@ -3,9 +3,27 @@ import numpy as np
 from micropsi_core.world.world import World
 from micropsi_core.world.worldadapter import ArrayWorldAdapter
 
+"""
+World and worldadapter to allow processing a fixed data array.
 
-# basic structure: the world holds a dataset in memory
-# worldadapters implement different ways of splitting and iterating over that dataset.
+Basic structure: The world holds a reference to a data archive (currently numpy npz, later perhaps also hdf5)
+Different worldadapters implement different ways of splitting and iterating over this data.
+
+----
+
+Some considerations for larger-than-RAM-datasets:
+
+These worldadapters do only basic slicing (indexing operations that can be described by 'start:stop:step').
+If the data is a numpy archive, this means that only array views are created, no copies.
+If it is a HDF5 archive, as far as I know, these operations do not even cause the data to be read from disk.
+
+This ends only at the point where a minibatch is created, which involves integer based fancy indexing (requiring copying).
+
+So this setup will need `size of full dataset + 1 minibatch` of RAM if the data comes in a numpy archive,
+and unless I'm mistaken `size of 1 minibatch` if comes in a Hdf5 archive.
+
+Caveat: I have not tested this.
+"""
 
 
 class Dataset(World):
@@ -14,33 +32,106 @@ class Dataset(World):
     def __init__(self, filename, world_type="Dataset", name="", owner="", engine=None, uid=None, version=1, config={}):
         World.__init__(self, filename, world_type=world_type, name=name, owner=owner, uid=uid, version=version)
 
-        archive = np.load(config['path'])
-
+        self.archive = np.load(config['path'])
+        # todo: allow hdf5 archives, too
 
     @classmethod
     def get_config_options(cls):
         return [
             {'name': 'path',
-             'description': 'file containing the data set',
+             'description': 'path of a numpy archive',
              'default': ''}
-        ]
+            ]
 
-
-# ideally, WAs operate as much as possible with _views_ (i.e. only slice-based indexing)
 
 class SupervisedLearning(ArrayWorldAdapter):
-    # configurable train, test, validation split
-    # selection of input and target variables, exposed in separate flow datasources
-    # iterates over the shuffled training set in mini batches: two ever changing data sources
-    # keeps validation and test set fixed in separate data sources: four unchanging datasources (should produce 0 overhead)
 
+    @classmethod
+    def get_config_options(cls):
+        return [
+             {'name': 'input_key',
+             'description': 'key under which the Nxk matrix of input (predictor) variables is found'
+             'default': 'x'},
+             {'name': 'target_key',
+             'description': 'key under which the Nxl matrix of target variables is found',
+             'default': 'y'},
+             {'name':'holdout_fraction',
+             'description': 'ratio of training to holdout data',
+             'default': 0.75},
+             {'name': 'batch_size',
+             'description': '(optional) provide only a certain number of samples from the training set in each world step',
+             'default': 500}
+
+            ]
+
+    def __init__(self, world, uid=None, **data):
+        super().__init__(world, uid, **data)
+
+        self.rng = np.random.RandomState(seed=1)
+
+        X = world.archive[self.input_key]
+        Y = world.archive[self.target_key]
+        assert X.shape[0] == Y.shape[0]
+        self.N = X.shape[0]
+        self.k = X.shape[1]
+        self.l = Y.shape[1]
+
+        self.len_train = int(self.N*self.holdout_fraction)
+        self.train_X = self.X[:self.len_train, :]
+        self.train_Y = self.Y[:self.len_train, :]
+
+        test_X = self.X[self.len_train:, :]
+        test_Y = self.Y[self.len_train:, :]
+
+        self.epoch_count = 0 # nr of times the whole training set has been seen
+        self.batch_start = 0 # row idx where the current batch begins
+        self.max_batch_start = self.N - self.batch_size # last row idx where we can start a batch
+
+        if not self.batch_size:
+            self.batch_size = self.len_train
+
+        self.add_flow_datasource("train_X", shape=(self.batch_size, self.k))
+        self.add_flow_datasource("train_Y", shape=(self.batch_size, self.l))
+
+        self.add_flow_datasource("test_X", shape=(self.N - self.len_train, self.k))
+        self.add_flow_datasource("test_Y", shape=(self.N - self.len_train,, self.l))
+
+        self.add_flow_datasource("epoch_count", shape=1)
+
+        # the contents of the test set never change, so we can write to this datasource
+        # at init time and never touch it again.
+        self.set_flow_datasource("test_X", test_X)
+        self.set_flow_datasource("test_Y", test_Y)
+
+    def shuffle(self):
+        self.shuffle_order = self.rng.permutation(self.len_train)
+
+    def batch_step(self):
+        if self.batch_start < self.max_batch_start:
+            self.batch_start += self.batch_size
+        else:
+            self.shuffle()
+            self.batch_start = 0
+            self.epoch_count += 1
+
+        batch_idxs = self.shuffle_order[self.batch_start:self.batch_start+self.batch_size]
+        return batch_idxs
+
+
+    def update_data_sources_and_targets(self):
+        batch_idxs = self.batch_step()
+
+        batch_X = self.train_X[batch_idxs, :]
+        batch_Y = self.train_Y[batch_idxs, :]
+
+        self.set_flow_datasource("train_X", batch_X)
+        self.set_flow_datasource("train_Y", batch_Y)
+        self.set_flow_datasource("epoch_count", self.epoch_count)
 
 class UnsupervisedLearning(ArrayWorldAdapter):
-    # configurable train, test, validation split
-    # selection of interesting variables
-    # iterates over the shuffled training set in mini batches: one ever changing data source
-    # keeps validation and test set fixed in separate data sources: two unchanging datasources (should produce 0 overhead)
-
+    # like Supervised, but without Y datasource
+    pass
 
 class TimeseriesPrediction(ArrayWorldAdapter):
-    # like unsupervised, but doesnt shuffle and adds option for temporal embedding (with special attention to temporal discontinuities) [the embedded data matrix can optionally be shuffled again]
+    # adds the option for temporal embeddings (with special attention to time discontinuities)
+    pass

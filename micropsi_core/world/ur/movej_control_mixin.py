381c00524357d2474b191634b7577fb13460488b
Ronnie Vuine <wintermute@tessierashpool.net>
2017-01-20 20:08:22 +0100

    introducing an „execute“ data target

--- micropsi_core/world/ur/movej_control_mixin.py
+++ micropsi_core/world/ur/movej_control_mixin.py
@@ -33,39 +33,44 @@ class MoveJControlMixin(WorldAdapterMixin):
         self.add_datatarget("tip-pos-ry")
         self.add_datatarget("tip-pos-rz")
 
+        self.add_datatarget("execute")
         self.add_datasource("in-target")
 
     def write_to_world(self):
         super().write_to_world()
 
-        flow_target = self.get_flow_datatarget("tip-pos")
+        if self.get_datatarget_value("execute") > 0.9:
 
-        self.last_target = [
-            self.get_datatarget_value('tip-pos-x') + flow_target[0],
-            self.get_datatarget_value('tip-pos-y') + flow_target[1],
-            self.get_datatarget_value('tip-pos-z') + flow_target[2],
-            self.get_datatarget_value('tip-pos-rx') + flow_target[3],
-            self.get_datatarget_value('tip-pos-ry') + flow_target[4],
-            self.get_datatarget_value('tip-pos-rz') + flow_target[5]
-        ]
+            flow_target = self.get_flow_datatarget("tip-pos")
 
-        command = "movej(p[%.4f, %.4f, %.4f, %.4f, %.4f, %.4f], %.4f, %.4f)" % (
-            self.last_target[0],
-            self.last_target[1],
-            self.last_target[2],
-            self.last_target[3],
-            self.last_target[4],
-            self.last_target[5],
-            float(self.acceleration),
-            float(self.velocity)
-        )
-        command += os.linesep
-        self.world.connection_daemon.write_command_to_robot(command)
+            self.last_target = [
+                self.get_datatarget_value('tip-pos-x') + flow_target[0],
+                self.get_datatarget_value('tip-pos-y') + flow_target[1],
+                self.get_datatarget_value('tip-pos-z') + flow_target[2],
+                self.get_datatarget_value('tip-pos-rx') + flow_target[3],
+                self.get_datatarget_value('tip-pos-ry') + flow_target[4],
+                self.get_datatarget_value('tip-pos-rz') + flow_target[5]
+            ]
+
+            command = "movej(p[%.4f, %.4f, %.4f, %.4f, %.4f, %.4f], %.4f, %.4f)" % (
+                self.last_target[0] / 1000,
+                self.last_target[1] / 1000,
+                self.last_target[2] / 1000,
+                self.last_target[3],
+                self.last_target[4],
+                self.last_target[5],
+                float(self.acceleration),
+                float(self.velocity)
+            )
+            command += os.linesep
+
+            self.world.connection_daemon.write_command_to_robot(command)
 
     def read_from_world(self):
         super().read_from_world()
 
-        in_target = sum(np.abs(self.world.connection_daemon.tool_pos_6D - self.last_target)) < 0.01
+        diff = self.world.connection_daemon.tool_pos_6D - self.last_target
+        in_target = sum(np.abs(diff)) < 0.01
         self.set_datasource_value("in-target", 1 if in_target else 0)
 
     def shutdown(self):

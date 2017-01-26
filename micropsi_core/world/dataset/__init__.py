609ef3304af7d502b4d1632ae3a0009bf8ffee15
Dominik Welland <hallo@doik.de>
2017-01-26 14:52:00 +0100

    remove worlds from repo

--- micropsi_core/world/dataset/__init__.py
+++
@@ -1,3 +0,0 @@
-
-# list all files containing world classes
-__all__ = ["data_world"]
--- micropsi_core/world/ur/optoforce/include/omd/opto.h
+++
@@ -1,8 +0,0 @@
-#ifndef OMD__OPTO_H
-#define OMD__OPTO_H 
-#include "omd/optopackage.h"
-#include "omd/optopackage6d.h"
-#include "omd/sensorconfig.h"
-#include "omd/optodaq.h"
-#include "omd/optoports.h"
-#endif

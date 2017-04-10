55a46c2b3b97322675a1d6c2db1821cb693af641
cknd <clemens@micropsi-industries.com>
2017-04-10 14:41:43 +0200

    use the coloredlogs package

--- micropsi_core/micropsi_logger.py
+++ micropsi_core/micropsi_logger.py
@@ -7,6 +7,7 @@ __author__ = 'rvuine'
 
 import os
 import logging
+import coloredlogs
 import time
 from operator import itemgetter
 
@@ -101,6 +102,7 @@ class MicropsiLogger():
         if self.filehandler:
             logging.getLogger(name).addHandler(self.filehandler)
         self.loggers[name].debug("Logger %s ready" % name)
+        coloredlogs.install(logger=self.loggers[name])
 
     def unregister_logger(self, name):
         logging.getLogger(name).removeHandler(self.handlers[name])

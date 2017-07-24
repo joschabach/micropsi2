478ac813ac5c08675587e3f9693562394b2cce93
Ronnie Vuine <wintermute@tessierashpool.net>
2017-07-24 20:06:35 +0200

    Preparing for vision: assert floats, don’t convert

--- micropsi_core/world/worldadapter.py
+++ micropsi_core/world/worldadapter.py
@@ -357,17 +357,20 @@ try:
 
         def set_flow_datasource(self, name, values):
             """Set the values of the given flow_datasource """
-            values = np.array(np.reshape(values, self.flow_datasources[name].shape), dtype=self.floatX)
+            assert values.dtype == self.floatX
+            assert self.flow_datasources[name].shape == values.shape
             self.flow_datasources[name] = values
 
         def add_to_flow_datatarget(self, name, values):
             """Add the given values to the given flow_datatarget """
-            values = np.array(np.reshape(values, self.flow_datatargets[name].shape), dtype=self.floatX)
+            assert values.dtype == self.floatX
+            assert self.flow_datasources[name].shape == values.shape
             self.flow_datatargets[name] += values
 
         def set_flow_datatarget_feedback(self, name, values):
             """Set the values of the given flow_datatarget_feedback """
-            values = np.array(np.reshape(values, self.flow_datatarget_feedbacks[name].shape), dtype=self.floatX)
+            assert values.dtype == self.floatX
+            assert self.flow_datasources[name].shape == values.shape
             self.flow_datatarget_feedbacks[name] = values
 
         def set_datasource_values(self, values):

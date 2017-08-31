a83131bf775031bde3e18623a1f0138ea42e919e
Dominik Welland <hallo@doik.de>
2017-08-31 19:55:36 +0200

    unset entries if node definition missing

--- micropsi_core/nodenet/theano_engine/theano_partition.py
+++ micropsi_core/nodenet/theano_engine/theano_partition.py
@@ -1021,7 +1021,11 @@ class TheanoPartition():
             if self.allocated_nodes[id] > MAX_STD_NODETYPE:
                 uid = node_to_id(id, self.pid)
                 if uid in nodes_data:
-                    self.allocated_nodes[id] = get_numerical_node_type(nodes_data[uid]['type'], self.nodenet.native_modules)
+                    try:
+                        self.allocated_nodes[id] = get_numerical_node_type(nodes_data[uid]['type'], self.nodenet.native_modules)
+                    except ValueError:
+                        self.allocated_nodes[id] = 0
+                        self.allocated_elements_to_nodes[np.where(self.allocated_elements_to_nodes == id)] = 0
             if self.allocated_nodes[id] > MAX_STD_NODETYPE:
                 self.native_module_instances[uid] = self.nodenet.get_node(uid)
             elif self.allocated_nodes[id] == COMMENT:

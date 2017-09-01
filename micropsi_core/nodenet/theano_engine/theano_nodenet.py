32f4d176af2c46c3f4ea5986ad380a95cd488814
Dominik Welland <hallo@doik.de>
2017-09-01 13:02:35 +0200

    also remove entries from linkweight matrix

--- micropsi_core/nodenet/theano_engine/theano_nodenet.py
+++ micropsi_core/nodenet/theano_engine/theano_nodenet.py
@@ -584,12 +584,16 @@ class TheanoNodenet(Nodenet):
                 partition.load_data(nodes_data)
 
             if invalid_uids:
+                w_matrix = partition.w.get_value()
                 for uid in invalid_uids:
                     partition = self.get_partition(uid)
                     id = node_from_id(uid)
                     partition.allocated_nodes[id] = 0
                     partition.allocated_node_parents[id] = 0
+                    els = partition.allocated_elements_to_nodes[np.where(partition.allocated_elements_to_nodes == id)]
+                    w_matrix[els] = 0
                     partition.allocated_elements_to_nodes[np.where(partition.allocated_elements_to_nodes == id)] = 0
+                partition.w.set_value(w_matrix)
 
             for partition in self.partitions.values():
                 partition.load_inlinks()

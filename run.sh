aee019659ffe5fc141d7e51530855fd6795dc264
cknd <clemens@micropsi-industries.com>
2017-11-08 17:21:55 +0100

    add option to start the jupyter console with the run script

--- run.sh
+++ run.sh
@@ -3,7 +3,16 @@
 
 if [[ -a bin/activate ]]; then
 	source bin/activate
-	./start_micropsi_server.py
-else
-	./start_micropsi_server.py
 fi
+
+
+# with --console, start jupyter console as well
+if [ "$1" == '--console' ]; then
+(
+	sleep 2
+	jupyter qtconsole --existing --gui-completion='ncurses' --style fruity
+)&
+fi
+
+./start_micropsi_server.py
+
